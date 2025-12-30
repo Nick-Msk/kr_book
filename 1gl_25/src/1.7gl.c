8d75c289d2bd02199d33fcd9d3564d8d54efa681
Nick-Msk <vmksk.cur@gmail.com>
2025-12-30 21:27:03 +0300

    KR: gl 1.7 Virt method is added

--- 1gl_25/src/1.7gl.c
+++ 1gl_25/src/1.7gl.c
@@ -3,20 +3,35 @@
 
 long     power(int, int);
 
+long     vpower(int, int);
+
 int     main(int argc, const char *argv[]){
     int maxcnt = 10;
     if (argc > 1)
         maxcnt = atoi(argv[1]);
     for (int i = 0; i < maxcnt; i++)
-        printf("%d - %ld - %ld\n", i, power(2, i), power(-3, i));
+        printf("%d - %ld - %ld\n", i, vpower(2, i), vpower(-3, i));
     return 0;
 }
 
 long    power(int x, int n){
-    long p = 1;
+    long p;
 
-    for (int i = 1; i <= n; i++)
-        p = p * x;  // TODO: what's the fast way from Virt?
+    //for (int i = 1; i <= n; i++)
+      //  p = p * x;  // TODO: what's the fast way from Virt?
+    for (p = 1; n > 0; --n)
+        p = p * x;
     return p;
 }
 
+// Virt
+long    vpower(int x, int n){
+    long y = 1L;
+    while (n > 0){
+        if (n % 2 == 1)
+            y *= x;
+        x *= x;
+        n /= 2;
+    }
+    return y;
+}
