d7eb7bd8f6dc3044c98430c42e2901e7e11c1b39
Nick-Msk <vmksk.cur@gmail.com>
2025-12-30 21:55:18 +0300

    KR: gl 1.9

---
+++ 1gl_25/src/1.9gl.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static const int        MAXLINE = 10000;
+
+static int      get_line(char s[], int lim);
+static int      copy(char to[], const char from[]);
+
+int             main(int argc, const char *argv[]){
+    int     len, max = 0;
+    char    line[MAXLINE];
+    char    longest[MAXLINE];
+
+    while ( (len = get_line(line, MAXLINE) > 0)){
+        if (len > max){
+            max = len;
+            copy(longest, line);
+        }
+    }
+    if (max > 0)
+        printf("%s", longest);
+
+    return 0;
+}
+
+static int      get_line(char s[], int lim){
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+    if (c == '\n'){
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+static int      copy(char to[], const char from[]){
+    int i = 0;
+    while ((to[i] = from[i]) != '\0')
+        i++;
+    return i;
+}
