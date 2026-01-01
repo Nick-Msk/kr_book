0165ffc2bd16b0f9233ce94e7ce6cf673c01e94b
Nick-Msk <vmksk.cur@gmail.com>
2026-01-01 17:19:20 +0300

    KR: task 1.20 detab fix

--- 1gl_25/src/1.20.task.c
+++ 1gl_25/src/1.20.task.c
@@ -6,10 +6,11 @@ static int          detab(int tabsize);
 
 int                 main(int argc, const char *argv[]){
     int     tabsize = 8;    // default
-    if (argc < 1){
-        if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0)
+    if (argc > 1){
+        if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0){
             printf("%s detab task 1.20 KR\nUsage: %s <tabsize=8>\n", __FILE__, *argv);
-            return 0;
+            return 0;   
+        }
         tabsize = atoi(argv[1]);
     }
                     
@@ -18,16 +19,27 @@ int                 main(int argc, const char *argv[]){
     return 0;
 }
 
+static inline void	printspace(int shift){
+   	while (shift-- > 0)
+	  	putchar(' ');
+}
+
 static int          detab(int tabsize){
-    int tabcnt = 0, c;
+    int tabcnt = 0, c, pos = 0;
 
     while ( (c = getchar()) != EOF){
         if  (c == '\t'){
-            printf("%*c", tabsize - 1, c);
+			int shift = tabsize - (pos % tabsize); 
+            printspace(shift);
             tabcnt++;
         }
-        else
+        else { 
+            if (c == '\n')
+                pos = 0;
+            else 
+                pos++;
             putchar(c);
+        }
     }
     return tabcnt;
 }
