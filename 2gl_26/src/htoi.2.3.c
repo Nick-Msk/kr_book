36de15941734384c24510d876e22ead915a0c25a
Nick-Msk <vmksk.cur@gmail.com>
2026-01-05 11:34:19 +0300

    KR: task 2.3 hsoi upd1

--- 2gl_26/src/htoi.2.3.c
+++ 2gl_26/src/htoi.2.3.c
@@ -2,10 +2,15 @@
 #include <strings.h>
 #include <ctype.h>
 
+#include "log.h"
+
 // htoi
 static int              htoi(const char *s);
 
 int                     main(int argc, const char *argv[]){
+    static const char *logfilename = "log/htoi.2.3.log";
+    loginit(logfilename, false, 0, "Start");    // TODO: rework that to LOG("logdir") or LOGAPPEND("logdir") or LOGSWITCH("logdir") 
+
    if (argc > 1){
         if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0){
             printf("%s htoi KR task 2.3\nUsage: %s <0x0-9,A-F>\n", __FILE__, *argv);
@@ -14,6 +19,8 @@ int                     main(int argc, const char *argv[]){
     }
     int res = htoi(argv[1]);
     printf("%d\n", res);
+
+    logclose("...");
     return 0;
 }
 
@@ -22,29 +29,39 @@ int                     main(int argc, const char *argv[]){
 
 
 static inline int       toxdigit(char c){
+    logsimple("[%c]", c);
     c = tolower(c);
     if (c >= '0' && c <= '9')
-        return c - '0';
+        c -= '0';
     if (c >= 'a' && c <= 'f')
-        return c - 'a';
-   return c; 
+        c -= 'a' - 10;
+   return logsimpleret(c, "ret [%d]", c);
 }
 
 // htoi
-// 0x123AF4 
+// 0x123AF4
 static int            htoi(const char *s){
     int res = 0;
-     
+    int sign = 1;
+
+    while (isspace(*s))
+        s++;
+
+    if (*s == '-')
+        sign = -1, s++;
+    else if (*s == '+')
+        s++;
     if (*s == '0'){
         s++;
-        if (tolower(*s) != 'x') 
+        if (tolower(*s) != 'x')
             return 0;   // wrong format
         else
             s++;
     }
     while (isxdigit(*s)){
-        res = res * 16 + toxdigit(*s++);
+        res = res * 16 + toxdigit(*s);
+        s++;
     }
-    return res;
+    return res * sign;
 }
 
