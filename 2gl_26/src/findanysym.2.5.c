2fbe4af58141f68202e67665c65708315b7df77c
Nick-Msk <vmksk.cur@gmail.com>
2026-01-07 20:59:19 +0300

    KR: task 2.5, new method is creating NO_BUILD for now

--- 2gl_26/src/findanysym.2.5.c
+++ 2gl_26/src/findanysym.2.5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <strings.h>
+#include <stdlib.h>
 
 #include "metric.h"
 #include "log.h"
@@ -7,7 +8,7 @@
 #include "common.h"
 
 enum FindType {
-    FIND_ONEBYONE = 0,
+    FIND_ONEBYONE       = 0,
     FIND_SORTED_PATTERN = 1
 };
 
@@ -21,6 +22,33 @@ static inline const char *getFindTypeName(enum FindType t){
 
 static int                       findanysym(const char *restrict str, const char *restrict pt, enum FindType ty);
 
+// to common.c!!! TODO:
+char                    *read_from_file(FILE *f, int cnt /* not used for now*/, int p_cnt){
+    logenter("read from input (%d)");
+    int      sz = 1024, len, sum = 0;
+    char    *s = 0;        // string to store
+    s = malloc(sz);
+    if (!s){
+        fprintf(stderr, "Unable to acclocate %d\n", sz);
+        return 0;
+    }
+    while ((len = fread(s, 1, sz - 1, f)) > 0){
+        sum += len;
+        if (len == sz - 1){
+            s = realloc(s, sz *= 2);
+            if (!s){
+                fprintf(stderr, "Unable to acclocate %d\n", sz);
+                free(s);
+                return 0;
+            }
+        }
+    }
+    s[sum] = '\0';      // to make a normal c-string
+    if (p_cnt)
+        *p_cnt = sum;
+    return logret(s, "%d bytes were read", sum);
+}
+
 int                       main(int argc, const char *argv[]){
 
     static const char *logfilename = "log/findanysym.2.5.log";
@@ -29,26 +57,69 @@ int                       main(int argc, const char *argv[]){
     enum FindType ty = FIND_ONEBYONE;
     if (argc > 1){
         if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0){
-            printf("%s any string KR task 2.5\nUsage: %s <str1> <str2>\n", __FILE__, *argv); // Usage here??
+            printf("%s any string KR task 2.5\nUsage: %s <pattern> <method F/S>\n", __FILE__, *argv); // Usage here??
             return 0;
         }
     }
-    if (!check_arg(2, "Usage: %s <str1> <str2>\n", *argv)){
+    if (!check_arg(2, "Usage: %s <pattern> <method F/S>\n", *argv)){
         return 1;
         }
-    char        *str = strdup(argv[1]);
-    const char  *pt = argv[2];
+
+    if (toupper(*argv[2]) == 'F')
+        ty = FIND_ONEBYONE;
+    else if (toupper(*argv[2]) == 'S')
+        ty = FIND_SORTED_PATTERN;
+    const char  *pt = strdup(argv[1]);
     Metric *m = 0;
-    
+    int cnt = 0;
+    char *s = fead_from_file(stdin, cnt, &cnt);
+
+    if (!s){
+        fprintf(stderr, "Unable to read from input source\n");
+        return 2;
+    }
+
     if (ty == FIND_ONEBYONE)
         m = metric_get("FIND_ONEBYONE_cnt", true);
 
     printf("Found pos %d\n", findanysym(str, pt, ty));
 
     metric_print(m);
+    free(s);
 
     logclose("...");
     return 0;
+}
+
+static char                      *uniq_srt(char *s, int *p_len){
+    logsenter("str [%s]", s);
+    bool    hash[256] = {false};
+    int     i = 0, j = 0;
+    char    c;
+    while ( (c = s[j]) != '\0'){
+        if (!hash[c]){
+            hash[c] = true;
+            s[j++] = c;
+        }
+        i++;
+    }
+    s[j] = '\0';
+    if (p_len)
+        *p_len = j;
+    return logret(s, "new len = %d, new str[%s]", j, s);
+}
+
+// to common.c          TODO:
+int                             char_cmp(const void *s1, const void *s2){
+    const char *c1 = s1;
+    const char *c2 = s2;
+    return *c1 - *c2;
+}
+
+static char                     *sort_str(char *s, int len){
+    logenter("[%s]:%d", s, len);
+    qsort(s, len, 1, char_cmp);
+    return logret(s, "[%s]", s);
 }
 
 static int                       findanysym_onebyne(const char *restrict str, const char *restrict pt){
@@ -63,6 +134,7 @@ static int                       findanysym_onebyne(const char *restrict str, co
     return -1;
 }
 
+// just a launcher
 static int                       findanysym(const char *restrict str, const char *restrict pt, enum FindType ty){
     logenter("method %s, pattter [%s]", getFindTypeName(ty), pt);
     int  pos = -1;
