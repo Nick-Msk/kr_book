ac20e1e316b9c869d778b8316a2c02a51c525773
Nick-Msk <vmksk.cur@gmail.com>
2026-01-15 19:45:23 +0300

    KR: invert 2.7 init

---
+++ 2gl_26/src/invert.2.7.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "log.h"
+#include "common.h"
+#include "checker.h"
+
+static int              invert(unsigned x, unsigned p, unsigned n);
+
+int                     main(int argc, const char *argv[]){
+    static const char *logfilename = "log/invert.2.7.log";
+    loginit(logfilename, false, 0, "Start");    // TODO: rework that to LOG("logdir") or LOGAPPEND("logdir") or LOGSWITCH("logdir") 
+
+    if (argc > 1){
+        if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0){
+            printf("%s invert KR task 2.7 (input:  stdin)\nUsage: %s x, p, n (all unsigned)\n", __FILE__, *argv); // Usage here??
+            return 0;
+        }
+    }
+    if (!check_arg(4, "Usage: %s x, p, n (all unsigned)\n", *argv) ){
+        return 1;
+        }
+    int x, p, n;
+    x = atoi(argv[1]);
+    p = atoi(argv[2]);
+    n = atoi(argv[3]);
+
+    if (!inv (x >= 0 && p >= 0 && n >= 0, "must be positive") )
+        return 2;
+
+    int res = invert(x, p, n);
+    printf("Res = %d\n", res);
+    print_bits("x=", x);
+    print_bits("Bits=", res);
+
+    logclose("...");
+    return 0;
+}
+
+static int          invert(unsigned x, unsigned p, unsigned n){
+
+    char        buf[100];
+    logenter("x = %s", bits_str(buf, sizeof(buf), x) );
+/*
+    unsigned    mask = ~0;
+    unsigned    i = n;
+    while (i > 0){
+        mask &= ~(1 << (p + --i));
+    }
+    x &= mask;
+    fprint_bits(logfile, "x & mask ", x);
+
+    i = n; mask = 0;
+    while (i-- > 0)
+        //mask &= ~(1 << (0 + --i));
+        mask = (mask << 1) | 0x1; 
+
+    y = (y & mask) << p;
+    fprint_bits(logfile, "masked and shifted y=", y);
+    x |= y; */
+
+    return logret(x, "res = %s", bits_str(buf, sizeof(buf), x));
+}
+
