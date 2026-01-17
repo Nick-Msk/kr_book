77a8ab2e054898701bf9bc29846e9f186f051783
Nick-Msk <vmksk.cur@gmail.com>
2026-01-17 22:44:33 +0300

    KR: binsearch, task 3.1, array API moved to array.c

--- 3gl_26/src/binsearch.3.1.c
+++ 3gl_26/src/binsearch.3.1.c
@@ -7,118 +7,16 @@
 #include "common.h"
 #include "checker.h"
 #include "metric.h"
-
-// TODO: 2 array.h
-typedef struct {
-    int     len;
-    int     sz; // total size, > len + 1
-    int    *v;
-} IArray;
-
-// array, but not IArray, because common for int and double
-typedef enum ArrayFillType{
-    ARRAY_NONE = 0,
-    ARRAY_DESC,
-    ARRAY_ACS,
-    ARRAY_RND,
-    ARRAY_ZERO
-} ArrayFillType;
-
-static inline const char        *ArrayFillTypeName(ArrayFillType t){
-    switch (t){
-        CASE_RETURN(ARRAY_NONE);
-        CASE_RETURN(ARRAY_DESC);
-        CASE_RETURN(ARRAY_ACS);
-        CASE_RETURN(ARRAY_RND);
-        CASE_RETURN(ARRAY_ZERO);
-        default: return "";
-    }
-}
-
-
-#define                 IArray_init(...) (IArray){.len = 0, .sz = 0, .v = 0}
-
-// CREATE  and fill with method
-// to array.h
-IArray                  IArray_create(int cnt, ArrayFillType typ){
-    logenter("cnt %d, typ %s", cnt, ArrayFillTypeName(typ));
-    IArray      res = IArray_init();
-    int         initval;
-
-    res.sz    = round_up_2(cnt);   // 2^(x + 1)
-    res.len   = cnt;
-    res.v   = malloc(res.sz * sizeof(int));    // TODO: sysraise should be here!!!
-    // fill
-    switch (typ){
-        case ARRAY_DESC:
-            initval = 100 * res.len;   // hope it'll ne owerwelhm int
-            int     dec_value = 10;          // for now!!! It'll be changed
-            for (int i = 0; i < res.len; i++)
-                res.v[i] = initval -= rndint(dec_value);
-        break;
-        case ARRAY_ACS:
-            initval = res.len / 10;
-            int     asc_value = 10;          // for now!!! It'll be changed
-            for (int i = 0; i < res.len; i++)
-                res.v[i] = initval += rndint(asc_value);
-        break;
-        case ARRAY_RND:
-        case ARRAY_ZERO:
-            for (int i = 0; i < res.len; i++){ // iter??? TODO:
-                switch (typ){
-                    case ARRAY_RND:
-                        res.v[i] = rndint(10 * res.len);
-                    break;
-                    case ARRAY_ZERO:
-                        res.v[i] = 0;
-                    break;
-                    default:
-                    break;
-                }
-            }
-        break;
-        case ARRAY_NONE:
-            // just do nothing
-        break;
-        default:
-            logmsg("Unsupported type %d", typ);
-        break;
-    }
-    return logret(res, "sz = %d", res.sz);
-}
-
-void                    IArray_free(IArray *val){
-    if (val && val->v){
-        free(val->v);
-        val->v = 0;
-    }
-}
-
-int                     IArray_fprint(FILE *f, IArray val, int limit){
-    int     array_rec_line = 20;        // TODO: should be external to be able to replace
-    int     cnt = 0, i;
-    limit = (limit == 0)? val.len : (limit < val.len) ? limit : val.len;
-
-    for (i = 0; i < limit; i++){
-        cnt += fprintf(f, "%6d\t", val.v[i]);
-        if ( (i + 1) % array_rec_line == 0)
-            cnt += fprintf(f, "\n");
-    }
-    if (i < val.len)
-        cnt += fprintf(f, "and more (%d) ...\n", val.len - i);
-    return cnt;
-}
-
-static inline int       IArray_print(IArray val, int limit){
-    return IArray_fprint(stdout, val, limit);
-}
+#include "array.h"
 
 // internal proc
 static int              binsearch_kr(int x, IArray arr);
 // as per task 3.1 KR
 static int              binsearch_typ2(int x, IArray arr);
 
-const char *usage_str = "Usage: %s <len:int> <value:int> <type of gen:A/D (asc/desc)\n";
+static int              force_testing(int iter, int size);
+
+const char *usage_str = "Usage: %s <len:int> <value:int> <type of gen A:asc T:force testing)\n";
 
 int                     main(int argc, const char *argv[]){
     static const char *logfilename = "log/binsearch.3.1.log";
@@ -138,37 +36,34 @@ int                     main(int argc, const char *argv[]){
     char    gen_type = toupper(*argv[3]);
     int     val = atoi(argv[2]);
 
-    if (!inv (arr_len > 0 && gen_type == 'A' || gen_type == 'D', "only acsending (A) or Descending (D) generator are supported") )
+    if (!inv (arr_len > 0 && gen_type == 'A' || gen_type == 'T', "only acsending (A) or (T): force testing  generator are supported") )
         return 2;
 
-    if (gen_type == 'A')
-        gen_type = ARRAY_ACS;
-    else
-        gen_type = ARRAY_DESC;
+    if (gen_type == 'A'){
+        srand(time(NULL));
+        IArray arr1 = IArray_create(arr_len, ARRAY_ACS);
+        IArray_print(arr1, 200);
 
-    //srand(time(NULL));
-    IArray arr1 = IArray_create(arr_len, gen_type);
-    IArray_print(arr1, 0);
+        Metric *a = metric_create("binsearch_kr");
 
-    Metric *a = metric_create("binsearch_kr");
+        int res = binsearch_kr(val, arr1);
+        if (res < 0)
+            printf("Not found\n");
+        else 
+            printf("Found pos = %d\n", res);
+ 
+        metric_print(a);
 
-    int res = binsearch_kr(val, arr1);
-    if (res < 0)
-        printf("Not found\n");
-    else 
-        printf("Found pos = %d\n", res);
-    
-    metric_print(a);
+        Metric *a2 = metric_create("binsearch_typ2");
+        res = binsearch_typ2(val, arr1);
+        if (res < 0)
+            printf("Type2: Not found\n");
+        else 
+            printf("Type2: Found pos = %d\n", res);
 
-    Metric *a2 = metric_create("binsearch_typ2");
-    res = binsearch_typ2(val, arr1);
-    if (res < 0)
-        printf("Type2: Not found\n");
-    else 
-        printf("Type2: Found pos = %d\n", res);
-
-    metric_print(a2);
-
+        metric_print(a2);
+    } else 
+        force_testing(val, arr_len);
     logclose("...");
     return 0;
 }
@@ -218,3 +113,10 @@ static int              binsearch_typ2(int x, IArray arr){
         return logret(-1, "Not found");
 }
 
+static int              force_testing(int iter, int size){
+    logenter("iter %d, size %d", iter, size);
+
+    int     res = 0;
+
+    return logret(res, "Total failed %d", res);
+}
