7b8229981f903471650b6da938ceb8a6c328d694
Nick-Msk <vmksk.cur@gmail.com>
2026-01-20 20:06:38 +0300

    KR: expand task 3.3

--- 3gl_26/src/expand.3.3.c
+++ 3gl_26/src/expand.3.3.c
@@ -10,10 +10,10 @@
 // internal proc
 static int             expand(char *restrict t, const char *restrict s, int sz);
 
-const char *usage_str = "Usage: %s <str_to_expand\n";
+const char *usage_str = "Usage: %s <str_to_expand>\n";
 
 int                     main(int argc, const char *argv[]){
-    static const char *logfilename = "log/expand.3..log";
+    static const char *logfilename = "log/expand.3.3.log";
     loginit(logfilename, false, 0, "Start");    // TODO: rework that to LOG("logdir") or LOGAPPEND("logdir") or LOGSWITCH("logdir")
 
     if (argc > 1){
@@ -33,17 +33,62 @@ int                     main(int argc, const char *argv[]){
     if (!inv(t != 0, "Unable to allocatd %d", sz) )
         return 2;
 
+    printf("ORIG: %s\n", s);
     expand(t, s, sz - 1);
+
+    printf("PROC: %s\n", t);
     free(t);
 
     logclose("...");
     return 0;
 }
 
+static int             putseries(char *s, char from, char to, int sz){
+    int     i = 0;
+    if ( (islower(from) && islower(to) ) || (isupper(from) && isupper(to) ) || (isdigit(from) && isdigit(to) ) )
+        while (--sz > 0 && from <= to)
+            s[i++] = from++;
+    else
+        i = -1; // non compatible
+    return i;
+}
+
 static int             expand(char *restrict t, const char *restrict s, int sz){
     logenter("[%s] - [%d]", s, sz);
-    int         pos = 0;
-    /// TODO:
+    int         i = 0, j = 0;
+    char        c, prev = (char) EOF, cstart;
+    bool        ready_to_series = false;
 
-    return logret(pos, "expanded to %d  chars", pos);
+    while (j < sz && ( (c = s[i]) != '\0')){
+        if (c != '-'){
+            if (isalnum(c) && ready_to_series){
+                int inc = putseries(t + j, cstart + 1, c, sz - j);          // + 1 because of first sym is already in t[]
+                if (inc == -1)  // incompatible, no change
+                    t[j++] = '-';
+                else {
+                    i++;
+                    j += inc;
+                }
+                logmsg("after series j %d, start [%c], end [%c], i %d, inc %d", j, cstart + 1, c, i, inc);
+                ready_to_series = false;
+            }
+            else
+                t[j++] = s[i++];
+        } else {        // (c == '-')
+            if (isalnum(prev)){
+                ready_to_series = true;
+                cstart = prev;     // start series if next char is COMPATIBLE to previous
+                i++;
+            } else {
+                if (ready_to_series)
+                    t[j++] = prev;
+                t[j++] = c; i++;
+                ready_to_series = false;
+            }
+        }
+        //logmsg("prev [%c], c [%c], i %d, j %d ", prev, c, i, j);
+        prev = c;
+    }
+    t[j] = '\0';
+    return logret(j, "expanded to %d  chars", j);
 }
