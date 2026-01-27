fe20eb1243a5c319705cc3321a5874ca1506eabd
Nick-Msk <vmksk.cur@gmail.com>
2026-01-27 20:47:04 +0300

    UTILS: array: tests are added

--- utils/src/array.c
+++ utils/src/array.c
@@ -119,105 +119,85 @@ int                     Array_fprint(FILE *f, Array val, int limit){
     return cnt;
 }
 
-/*
-void                    DArray_fill(DArray a, ArrayFillType typ){
-    double     initval;
-    // fill
-    switch (typ){
-        case ARRAY_DESC:
-            initval = 100 * a.len;   // hope it'll ne owerwelhm int
-            double     dec_value = 10.0;          // for now!!! It'll be changed
-            for (int i = 0; i < a.len; i++)
-                a.v[i] = initval -= rnddbl(dec_value);
-        break;
-        case ARRAY_ACS:
-            initval = a.len / 10;
-            double     asc_value = 10.0;          // for now!!! It'll be changed
-            for (int i = 0; i < a.len; i++)
-                a.v[i] = initval += rnddbl(asc_value);
-        break;
-        case ARRAY_RND:
-        case ARRAY_ZERO:
-            for (int i = 0; i < a.len; i++){ // iter??? TODO:
-                switch (typ){
-                    case ARRAY_RND:
-                        a.v[i] = rnddbl(10 * a.len);
-                    break;
-                    case ARRAY_ZERO:
-                        a.v[i] = 0;
-                    break;
-                    default:
-                    break;
-                }
-            }
-        break;
-        case ARRAY_NONE:
-            // just do nothing
-        break;
-        default:
-            logsimple("Unsupported type %d", typ);
-        break;
-    }
-}
-
-// CREATE  and fill with method
-// increase and shrink are reuiqred too
-DArray                  DArray_create(int cnt, ArrayFillType typ){
-    logenter("cnt %d, typ %s", cnt, ArrayFillTypeName(typ));
-    DArray      res = DArray_init();
-
-    res.sz    = round_up_2(cnt);   // 2^(x + 1)
-    res.len   = cnt;
-    res.v   = malloc(res.sz * sizeof(int));
-    if (!res.v){
-        fprintf(stderr, "Unable to allocate %d\n", res.sz);
-        res.sz = INT_MIN;   // userraisesig hehe TODO:
-    }
-    DArray_fill(res, typ);
-    return logret(res, "sz = %d", res.sz);
-}
-
-void                    DArray_free(DArray *val){
-    if (val && val->v){
-        free(val->v);
-        val->v = 0;
-    }
-}
-
-// -------------------------- (API) printers -----------------------
-
-int                     DArray_fprint(FILE *f, DArray val, int limit){
-    int     cnt = 0, i;
-    limit = (limit == 0)? val.len : (limit < val.len) ? limit : val.len;
-
-    for (i = 0; i < limit; i++){
-        cnt += fprintf(f, "%.8g\t", val.v[i]);
-        if ( ( (i + 1) % g_array_rec_line) == 0){
-            cnt += fprintf(f, "\n");
-        }
-    }
-    if (i < val.len)
-        cnt += fprintf(f, "and more (%d) ...\n", val.len - i);
-    return cnt;
-}
-*/
-
 // -------------------------------Testing --------------------------
 
 #ifdef ARRAYTESTING
 
-#include "testing.h"
+#include "test.h"
 #include <signal.h>
 
 // ------------------------- TEST 1 ---------------------------------
 
-// ------------------------- TEST 2 ---------------------------------
+static TestStatus
+tf1(const char *name)
+{
+    logenter("%s", name);
+    int         subnum = 0;
+    {
+        test_sub("subtest %d", ++subnum);
+        Array darr = DArray_create(100, ARRAY_ZERO);
+        if (darr.dv[99] != 0.0)
+            return logret(TEST_FAILED, "Element must be 0.0. but not %f", darr.dv[99]);
+        if (!Array_isvalid(darr))
+            return logret(TEST_FAILED, "Validation is failed");
+        Arrayfree(darr);
+        if (darr.dv != 0)
+            return logret(TEST_FAILED, "Array is'nt freed");
+    }
+    {
+        test_sub("subtest %d", ++subnum);
+        Array iarr = IArray_create(100, ARRAY_ZERO);
+        if (iarr.iv[99] != 0)
+            return logret(TEST_FAILED, "Element must be 0.0. but not %d", iarr.iv[99]);
+        if (!Array_isvalid(iarr))
+            return logret(TEST_FAILED, "Validation is failed");
+        Arrayfree(iarr);
+        if (iarr.dv != 0)
+            return logret(TEST_FAILED, "Array is'nt freed");
+    }
+    return logret(TEST_PASSED, "done"); // TEST_FAILED
+}
 
+// ------------------------- TEST 2 ---------------------------------
+static TestStatus
+tf2(const char *name){
+    logenter("%s", name);
+
+    int         subnum = 0;
+    {
+        test_sub("subtest %d", ++subnum);
+        Array darr = DArray_create(100, ARRAY_ACS);
+        for (int i = 0; i < darr.len - 1; i++)
+            if (darr.dv[i] > darr.dv[i + 1])
+                return logret(TEST_FAILED, "Violation for ACS gen: arr[%d] = %f > arr[%d+1] = %f", i, darr.dv[i], i, darr.dv[i + 1]);
+
+        test_sub("subtest %d", ++subnum);
+        Array_fill(darr, ARRAY_DESC);
+        for (int i = 0; i < darr.len - 1; i++)
+            if (darr.dv[i] < darr.dv[i + 1])
+                return logret(TEST_FAILED, "Violation for DESC gen: arr[%d] = %f < arr[%d+1] = %f", i, darr.dv[i], i, darr.dv[i + 1]);
+    }
+
+    {
+        test_sub("subtest %d", ++subnum);
+        Array iarr = IArray_create(100, ARRAY_ACS);
+        for (int i = 0; i < iarr.len - 1; i++)
+            if (iarr.iv[i] > iarr.iv[i + 1])
+                return logret(TEST_FAILED, "Violation for ACS gen: arr[%d] = %d > arr[%d+1] = %d", i, iarr.iv[i], i, iarr.iv[i + 1]);
+
+        test_sub("subtest %d", ++subnum);
+        Array_fill(iarr, ARRAY_DESC);
+        for (int i = 0; i < iarr.len - 1; i++)
+            if (iarr.iv[i] < iarr.iv[i + 1])
+                return logret(TEST_FAILED, "Violation for DESC gen: arr[%d] = %d < arr[%d+1] = %d", i, iarr.iv[i], i, iarr.iv[i + 1]);
+    }
+    return logret(TEST_PASSED, "done"); // TEST_FAILED
+}
 // -------------------------------------------------------------------
 int
 main(int argc, char *argv[])
 {
-    const char *logfilename = "checker.log";
+    const char *logfilename = "log/array.log";
 
     if (argc > 1)
         logfilename = argv[1];
@@ -225,15 +205,14 @@ main(int argc, char *argv[])
     loginit(logfilename, false, 0, "Starting");
 
     testenginestd(
-        testnew(.f2 = tf1, .num = 1, .name = "Simple invariant text"       , .desc = "", .mandatory=true)
-      , testnew(.f2 = tf2, .num = 2, .name = "Complex invariant test"      , .desc = "", .mandatory=true)
-      //, testnew(.f2 = f3, .num = 3, .name = "Interrupt raising test"        , .desc = "Exception test."                                                             , .mandatory=true)
-      //, testnew(.f2 = f4, .num = 4, .name = "System error test."            , .desc = "System error raising test (w/o exception)."  , .mandatory=true)
+        testnew(.f2 = tf1, .num = 1, .name = "Int/double creation/descr test"       , .desc = "", .mandatory=true)
+      , testnew(.f2 = tf2, .num = 2, .name = "Int/double filling test"              , .desc = "", .mandatory=true)
     );
 
-        logclose("end...");
+    logclose("end...");
     return 0;
 }
+
 
 
 #endif /* ARRAYTESTING */
