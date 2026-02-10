9dd3e9c9eb05a649ccf7539aab700c8c633eb624
Nick-Msk <vmksk.cur@gmail.com>
2026-02-10 17:59:33 +0300

    KR: tail task 5.13 init (NO_BUILD)

---
+++ 5gl_26/src/tail.5.13.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "log.h"
+#include "common.h"
+#include "checker.h"
+#include "bool.h"
+
+typedef struct Keys {
+    bool    lines;
+    bool    version;
+    // ...
+} Keys;
+
+#define                 Keysinit(...) (Keys){.lines = false, .version = false, __VA_ARGS__}
+
+static                  int parse_keys(const char *argv[], Keys *ke){
+    logenter("...");
+    int     argc = 1, params = 0;
+    if (!ke)
+        return logerr(-1, "Zero ke!!! Error!");
+    char    c;
+    while (*++argv != 0 && **argv == '-'){
+        logauto(*argv);
+        argc++;
+        while ( (c = *++argv[0]) )
+            switch (tolower(c)){
+                case 'v':
+                    if (!ke->version){
+                        ke->version = true;
+                        params++;
+                    }
+                break;
+                case 'n':
+                    if (!ke->line){
+                        ke->lines = true;
+                        params++;
+                    }
+                break;
+                default:    // probaly it's possible to ignore unknows parameters
+                    fprintf(stderr, "Illegal option [%c]\n", c);
+                    return logerr(-1, "Illegal [%c], params [%d] argc %d", c, params, argc);
+            }
+    }
+    return logret(argc, "params %d, argc %d", params, argc);
+}
+
+const char *usage_str = "Usage: %s -n<cnt:int>\n";
+
+int                     main(int argc, const char *argv[]){
+    static const char *logfilename = "log/"__FILE__".log";
+    loginit(logfilename, false, 0, "Start");    // TODO: rework that to LOG("logdir") or LOGAPPEND("logdir") or LOGSWITCH("logdir")
+
+    Keys ke = Keysinit();
+    argc = parse_keys(argv, &ke);
+
+    if (argc < 0) {
+        printf(usage_str, *argv);
+        return 1;
+    }
+    if (ke.version){
+        printf("%s KR tail task 5.13\n", __FILE__);
+        printf(usage_str, *argv);
+        return 0;
+    }
+
+    // TODO:
+
+    logclose("found %d", found);
+    return 0;
+}
+
+
