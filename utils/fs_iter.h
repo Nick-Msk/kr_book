d2eedbbb3cc557a3dd126a7f33c54310225eb5a6
Nick-Msk <vmksk.cur@gmail.com>
2026-02-19 23:22:35 +0300

    UTILS: fs_util: init (forward tests passed, backward - NOT)

--- utils/fs_iter.h
+++ utils/fs_iter.h
@@ -20,27 +20,84 @@
 
 // ------------------- TYPES -----------------------
 
-// faststring iterator
-typedef struct fs {
-    int         len, sz; // sz >= len + 1 because of last '\0'
-    FS_FLAGS    flags;
-#if defined(FS_ALLOCATOR)
-    int         pos;    //
-#endif
-    char       *v;      // with '\0'
-} fs;
+typedef struct fsiter {
+    int     pos, from, to;
+    char   *v;
+} fsiter;
+
+typedef struct fsiterrev {
+    int     pos, from, to;
+    char   *v;
+} fsiterrev;
 
 // ------------- CONSTRUCTOTS/DESTRUCTORS ----------
 
-#define FSEMPTY (fs){.sz = 0, .len = 0, .flags = FS_FLAG_STATIC, .v = ""};
+// TODO: make a REV interator as fsreviter{ };
 
-#define FSINITSTATIC(...)  (fs){.sz = 1, .len = 0, .flags = FS_FLAG_STATIC, .v = "", ##__VA_ARGS__}
+// forward iterator
+static inline fsiter        fseach(fs s){
+    return (fsiter){.pos = 0, .from = 0, .to = s.len, .v = s.v};
+}
 
-#define FS(...)   (fs){.sz = 0, .len = 0, .flags = FS_FLAG_ALLOC, .v = 0, ##__VA_ARGS__}
+// foprward iterator
+static inline fsiterrev     fseachrev(fs s){
+    return (fsiterrev){.pos = s.len - 1, .from = s.len - 1, .to = 0, .v = s.v};
+}
 
-#define fsfree(s) fs_free(&(s))
+// ++ slice, TODO: rev slice
+static inline fsiter        fsslice(fs s, int from, int to){
+    fsiter i = fseach(s);
+    i.from = i.pos = from < s.len ? from : s.len;
+    i.to = to < s.len ? to : s.len;
+    return i; // (fsiter){.pos = MIN(from, s.len), .from = MIN(from, s.len), .to = MIN(to, s.len), .v = s.v};
+}
 
 // -------------------- ACCESS AND MODIFICATORS ------------------------
+
+static inline bool          fshasnext(fsiter *i){
+    return i->pos < i->to;
+}
+
+static inline char         *fsnext(fsiter *i){
+    return i->v + i->pos++;
+}
+
+static inline char         *fscurr(fsiter *i){
+    return i->v + i->pos;
+}
+
+static inline bool          fshasnextrev(fsiterrev *i){
+    return i->pos > i->to;
+}
+
+static inline char         *fsnextrev(fsiterrev *i){
+    return i->v + i->pos--;
+}
+
+static inline char         *fscurrrev(fsiterrev *i){
+    return i->v + i->pos;
+}
+
+#define                     hasnext(i) fshasnext(&(i) )
+#define                     next(i) *(fsnext(&(i) ) )
+#define                     curr(i) *(fscurr(&(i) ) )
+#define                     fsforeach(s, i) for (fsiter i = fseach(s); hasnext(i); next(i) )
+
+#define                     hasnextrev(i) fshasnextrev(&(i) )
+#define                     nextrev(i) *(fsnextrev(&(i) ) )
+#define                     currrev(i) *(fscurrrev(&(i) ) )
+#define                     fsforeachrev(s, i) for (fsiterrev i = fseachrev(s); hasnextrev(i); nextrev(i) )
+
+// in future
+// #define lyamdba(s, compare) ...
+
+//
+// foreach(s, i)
+//     get(s, i) = 'u';
+// for (fsiter i = fsaech(s)/fsslice(s, 2, 11); hasnext(i); next(i))
+        // get(i) = 'y';    // or s.v[i.pos] = 't';
+// for (int i = 0; i < s.len; i++)
+//  s.v[i] = ...;
 
 // ------------------------ PRINTERS/CHECKERS --------------------------
 
--- utils/src/fs_iter.c
+++ utils/src/fs_iter.c
@@ -2,6 +2,7 @@
 #include "log.h"
 #include "fs.h"
 #include "error.h"
+#include "fs_iter.h"
 
 /********************************************************************
                     FAST STRING MODULE IMPLEMENTATION
@@ -41,22 +42,82 @@ tf1(const char *name)
     {
         test_sub("subtest %d", ++subnum);
 
+        const char *arr = "abcdefgh";
+        fs s = fsliteral(arr);
+        for (fsiter i = fseach(s); hasnext(i); next(i) )
+            if (curr(i) != *arr++)
+                return logerr(TEST_FAILED, "current iterator value [%c] != [%c]", curr(i), *(arr - 1));
+
+        test_sub("subtest %d", ++subnum);
+        fs      str = fsinit(100);
+        int     pos = 0;
+        for (fsiter i = fseach(s); hasnext(i); next(i) )
+            elem(str, pos++) = curr(i);
+        fsend(str, pos);
+        if (fscmp(str, s) != 0)
+            return logactret(fsfree(str), TEST_FAILED, "[%s] not equal to origin [%s]", fsstr(str), fsstr(s) );
+
+        test_sub("subtest %d", ++subnum);
+
+        pos = 0;
+        for (fsiter i = fsslice(s, 4, 11); hasnext(i); next(i) )
+            elem(str, pos++) = curr(i);
+        fsend(str, pos);
+        if (strcmp(s.v + 4, fsstr(str) ) != 0)
+            return logactret(fsfree(str), TEST_FAILED, "[%s] not equal to origin [%s]", fsstr(str), s.v + 4);
+        fsfree(str);
+    }
+    return logret(TEST_PASSED, "done"); // TEST_FAILED
+}
+
+// ------------------------- TEST 2 ---------------------------------
+
+static TestStatus
+tf2(const char *name)
+{
+    logenter("%s", name);
+    int         subnum = 0;
+    {
+        test_sub("subtest %d", ++subnum);
+
+        fs      str = fsinit(100);
+        const char *arr = "abcdefgh";
+        fs      s = fsliteral(arr);
+        int     pos = 0;
+        for (fsiterrev i = fseachrev(s); hasnextrev(i); nextrev(i) )
+            elem(str, pos++) = currrev(i);
+        fsend(str, pos);
+
+        printf("BEFORE REV: %s\n", fsstr(str) );
+        fs_reverse(str);
+
+        if (fscmp(str, s) != 0)
+            return logactret(fsfree(str), TEST_FAILED, " REVERSED [%s] not equal to origin [%s]", fsstr(str), fsstr(s) );
+        fsfree(str);
+    }
     return logret(TEST_PASSED, "done"); // TEST_FAILED
 }
 
 // ------------------------------------------------------------------
 
 int
-main(/* int argc, char *argv[] */)
+main(int argc, const char *argv[])
 {
-    logsimpleinit("Starting");   // it that working?
+    const char *logfilename = "log/fs_iter.log";
+
+    loginit(logfilename, false, 0, "Starting");
+
+    if (argc > 1)
+        logfilename = argv[1];
 
     testenginestd(
-        testnew(.f2 = tf1, .num = 1, .name = "Simple init and validate test" , .desc="Init test."                , .mandatory=true)
+        testnew(.f2 = tf1, .num = 1, .name = "Simple iteration test" ,          .desc="", .mandatory=true)
+      , testnew(.f2 = tf2, .num = 2, .name = "Reverse iteration test" ,         .desc="", .mandatory=true)
     );
 
     logclose("end...");
     return 0;
+
 }
 
 #endif /* FSITERTESTING */
