c6ad4942ccd3d323fb378588954451fc450a8851
Nick-Msk <vmksk.cur@gmail.com>
2026-02-20 21:59:12 +0300

    UTILS: fs_iter: auto construction is added (TEST_NOT_OK)

--- utils/fs_iter.h
+++ utils/fs_iter.h
@@ -30,6 +30,11 @@ typedef struct fsiterrev {
     char   *v;
 } fsiterrev;
 
+typedef struct fsnew {
+    int     pos;
+    fs     *s;
+} fsnew;
+
 // ------------- CONSTRUCTOTS/DESTRUCTORS ----------
 
 // TODO: make a REV interator as fsreviter{ };
@@ -42,6 +47,12 @@ static inline fsiter        fseach(fs s){
 // foprward iterator
 static inline fsiterrev     fseachrev(fs s){
     return (fsiterrev){.pos = s.len - 1, .from = s.len - 1, .to = 0, .v = s.v};
+}
+
+// new iterator, it's assumed str is initialized as heap!
+static inline fsnew         fsinew(fs *str){
+    // fs_init
+    return (fsnew){.pos = 0, .s = str};
 }
 
 // ++ slice, TODO: rev slice
@@ -67,7 +78,7 @@ static inline char         *fscurr(fsiter *i){
 }
 
 static inline bool          fshasnextrev(fsiterrev *i){
-    return i->pos > i->to;
+    return i->pos >= i->to;
 }
 
 static inline char         *fsnextrev(fsiterrev *i){
@@ -78,15 +89,36 @@ static inline char         *fscurrrev(fsiterrev *i){
     return i->v + i->pos;
 }
 
-#define                     hasnext(i) fshasnext(&(i) )
-#define                     next(i) *(fsnext(&(i) ) )
-#define                     curr(i) *(fscurr(&(i) ) )
+// semi-constructor!
+static inline char         *fselemnext(fsnew *i){
+    return fs_elem(i->s, i->pos++);
+}
+// not sure if really need that
+static inline char         *fselemcurr(fsnew *i){
+    return fs_elem(i->s, i->pos);
+}
+
+// setup len and '\0'
+static inline char         *fselemend(fsnew *i){
+    return fs_setlen(i->s, i->pos);
+}
+
+// forward
+#define                     hasnext(i)   fshasnext(&(i) )
+#define                     next(i)     *(fsnext(&(i) ) )
+#define                     curr(i)     *(fscurr(&(i) ) )
 #define                     fsforeach(s, i) for (fsiter i = fseach(s); hasnext(i); next(i) )
 
+// reverse
 #define                     hasnextrev(i) fshasnextrev(&(i) )
-#define                     nextrev(i) *(fsnextrev(&(i) ) )
-#define                     currrev(i) *(fscurrrev(&(i) ) )
+#define                     nextrev(i)   *(fsnextrev(&(i) ) )
+#define                     currrev(i)   *(fscurrrev(&(i) ) )
 #define                     fsforeachrev(s, i) for (fsiterrev i = fseachrev(s); hasnextrev(i); nextrev(i) )
+
+// fsnew
+#define                     elemnext(i) *fselemnext(&(i) )
+#define                     elemcurr(i) *fselemcurr(&(i) )
+#define                     elemend(i)  *fselemend(&(i) )
 
 // in future
 // #define lyamdba(s, compare) ...
@@ -94,6 +126,8 @@ static inline char         *fscurrrev(fsiterrev *i){
 //
 // foreach(s, i)
 //     get(s, i) = 'u';
+// for (... )
+//    elemnext(is) = arr[i] + '0';
 // for (fsiter i = fsaech(s)/fsslice(s, 2, 11); hasnext(i); next(i))
         // get(i) = 'y';    // or s.v[i.pos] = 't';
 // for (int i = 0; i < s.len; i++)
--- utils/src/fs_iter.c
+++ utils/src/fs_iter.c
@@ -44,16 +44,32 @@ tf1(const char *name)
 
         const char *arr = "abcdefgh";
         fs s = fsliteral(arr);
+
+        fs_techfprint(logfile, &s);    // for manual checking
+
+        if (fslen(s) != (int) strlen(arr) )
+             return logret(TEST_FAILED, "len = %d != strlen () = %lu", fslen(s), strlen(arr) );
+
         for (fsiter i = fseach(s); hasnext(i); next(i) )
             if (curr(i) != *arr++)
                 return logerr(TEST_FAILED, "current iterator value [%c] != [%c]", curr(i), *(arr - 1));
 
         test_sub("subtest %d", ++subnum);
+
         fs      str = fsinit(100);
         int     pos = 0;
-        for (fsiter i = fseach(s); hasnext(i); next(i) )
+        for (fsiter i = fseach(s); hasnext(i); next(i) ){
+            logmsg("pos %d, curr %c", pos, curr(i) );
             elem(str, pos++) = curr(i);
+        }
+        logmsg("last pos %d", pos);
         fsend(str, pos);
+
+        fs_techfprint(logfile, &str);    // for manual checking
+
+        if (fslen(str) != fslen(s) )
+            return logactret(fsfree(str), TEST_FAILED, "str.len = %d != s.len () = %d", fslen(str), fslen(str) );
+
         if (fscmp(str, s) != 0)
             return logactret(fsfree(str), TEST_FAILED, "[%s] not equal to origin [%s]", fsstr(str), fsstr(s) );
 
@@ -84,15 +100,47 @@ tf2(const char *name)
         const char *arr = "abcdefgh";
         fs      s = fsliteral(arr);
         int     pos = 0;
-        for (fsiterrev i = fseachrev(s); hasnextrev(i); nextrev(i) )
+        fsiterrev i;
+        for (i = fseachrev(s); hasnextrev(i); nextrev(i) )
             elem(str, pos++) = currrev(i);
         fsend(str, pos);
 
         printf("BEFORE REV: %s\n", fsstr(str) );
+        fs_techfprint(logfile, &str);
         fs_reverse(str);
 
         if (fscmp(str, s) != 0)
             return logactret(fsfree(str), TEST_FAILED, " REVERSED [%s] not equal to origin [%s]", fsstr(str), fsstr(s) );
+        fsfree(str);
+    }
+    return logret(TEST_PASSED, "done"); // TEST_FAILED
+}
+
+// ------------------------- TEST 3 ---------------------------------
+
+static TestStatus
+tf3(const char *name)
+{
+    logenter("%s", name);
+    int         subnum = 0;
+    {
+        test_sub("subtest %d", ++subnum);
+
+        fs      str = fsinit(100);
+        const char arr[] = "abc1234567890";
+
+        fsnew   it = fsinew(&str);
+        for (int i = 0; i < COUNT(arr); i++)
+            elemnext(it) = arr[i];
+        elemend(it);
+
+        fs_techfprint(logfile, &str);    // for manual checking
+        //logmsg("strlen: %lu, len %d", strlen(arr), fslen(str) );
+        if (fslen(str) != strlen(arr) )
+            return logactret(fsfree(str), TEST_FAILED, "len = %d != strlen () = %lu", fslen(str), strlen(arr) );
+
+        if (strcmp(fsstr(str), arr) != 0)
+            return logactret(fsfree(str), TEST_FAILED, "str [%s] not equal to origin arr [%s]", fsstr(str), arr );
         fsfree(str);
     }
     return logret(TEST_PASSED, "done"); // TEST_FAILED
@@ -113,6 +161,7 @@ main(int argc, const char *argv[])
     testenginestd(
         testnew(.f2 = tf1, .num = 1, .name = "Simple iteration test" ,          .desc="", .mandatory=true)
       , testnew(.f2 = tf2, .num = 2, .name = "Reverse iteration test" ,         .desc="", .mandatory=true)
+      , testnew(.f2 = tf3, .num = 3, .name = "Construct/iteration test" ,       .desc="", .mandatory=true)
     );
 
     logclose("end...");
