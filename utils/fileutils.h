889b57a237d2eb8cd8aceffec5dd2defd83de3e4
Nick-Msk <vmksk.cur@gmail.com>
2026-02-27 22:46:48 +0300

    UTILS: fileutils.h: readfs_file is added to pub

--- utils/fileutils.h
+++ utils/fileutils.h
@@ -12,15 +12,14 @@
 
 // ---------------------- API ----------------------------------------
 
-// for now in common.c, then will be moved out
-extern int                      get_line(char *line, int lim);
-
-// for now in common.c, then will be moved out
+// --------------- fs based API ---------------------------
 extern int                      fgetline_fs(FILE *restrict in, fs *restrict s);
 
 static inline int               getline_fs(fs *str){
     return fgetline_fs(stdin, str);
 }
+
+extern fs                       readfs_file(FILE *f);
 
 // read all (if maxline == -1 or maxline) lines separately into fs[]
 extern int                      freadlines(FILE *restrict f, fs **restrict lines);
@@ -29,6 +28,8 @@ static inline int               readlines(fs **lines){
     return freadlines(stdin, lines);
 }
 
+// OLD char * API
+extern int                      get_line(char *line, int lim);
 // write cnt fs into stream
 extern int                      fwritelines(FILE *restrict f, const fs *restrict ptr, int cnt);
 
