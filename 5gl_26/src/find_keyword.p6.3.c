ba577d4288e58bf3d61749890c6aafe43c248bef
Nick-Msk <vmksk.cur@gmail.com>
2026-03-06 20:30:45 +0300

    KR: find_keyword task p6.3 init

---
+++ 5gl_26/src/find_keyword.p6.3.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <strings.h>
+#include <ctype.h>
+
+#include "log.h"
+#include "checker.h"
+#include "common.h"
+#include "fileutils.h"
+#include "fs.h"
+#include "error.h"
+
+typedef struct Keys {
+    bool    version;
+    bool    sens;
+    // ...
+} Keys;
+
+#define                 Keysinit(...) (Keys){ .version = false, .sens = true, __VA_ARGS__}
+
+static int              parse_keys(const char *argv[], Keys *ke){
+    logenter("...");
+    int     argc = 1, params = 0;
+    if (!ke)
+        return userraiseint(-1, "Zero ke!!! Error!");   // raise here
+    char    c;
+    while (*++argv != 0 && **argv == '-'){
+        logauto(*argv);
+        argc++;
+        while ( (c = *++argv[0]) )
+            switch (tolower(c)){
+                case 'v':
+                    if (!ke->version){
+                        ke->version = true;
+                        params++;
+                    }
+                break;
+                case 'i':
+                    if (!ke->sens){
+                        ke->sens = false;
+                        params++;
+                    }
+                break;
+                default:    // probaly it's possible to ignore unknows parameters
+                    fprintf(stderr, "Illegal option [%c]\n", c);
+                    return logerr(-1, "Illegal [%c], params [%d] argc %d", c, params, argc);
+            }
+    }
+    return logret(argc, "params %d, argc %d", params, argc);
+}
+
+static const char   *usage_str = "Usage: %s -v -i\n";
+
+int                      main(int argc, const char *argv[]){
+    logsimpleinit("Start");
+
+    Keys    ke = Keysinit();
+    argc = parse_keys(argv, &ke);
+
+    if (argc < 0) {
+        printf(usage_str, *argv);
+        return 1;
+    }
+    if (ke.version){
+        printf("%s KR find_keyword p6.3\n", __FILE__);
+        printf(usage_str, *argv);
+        return 0;
+    }
+
+    return logret(0, "end...");  // as replace of logclose()
+}
+
