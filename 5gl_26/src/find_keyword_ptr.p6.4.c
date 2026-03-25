8917d207a4c0aabcccceaa86b1f71e5ff8ec47c3
Nick-Msk <vmksk.cur@gmail.com>
2026-03-25 11:32:52 +0300

    KR: find_keyword p6.4

---
+++ 5gl_26/src/find_keyword_ptr.p6.4.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <strings.h>
+#include <ctype.h>
+
+#include "log.h"
+#include "common.h"
+#include "fs.h"
+#include "fs_iter.h"
+#include "error.h"
+#include "buffer.h"
+
+typedef struct Keys {
+    bool    version;
+    bool    sens;
+    // ...
+} Keys;
+#define                 Keysinit(...) (Keys){ .version = false, .sens = true, __VA_ARGS__}
+
+static int              parse_keys(const char *argv[], Keys *ke){
+    logenter("...");
+    int     argc = 1, params = 0;
+    if (!ke)
+        return userraiseint(-1, "Zero ke!!! Error!");   // raise here
+    char    c;
+    while (*++argv != 0 && **argv == '-'){
+        logauto(*argv);
+        argc++;
+        while ( (c = *++argv[0]) )
+            switch (tolower(c)){
+                case 'v':
+                    if (!ke->version){
+                        ke->version = true;
+                        params++;
+                    }
+                break;
+                case 'i':
+                    if (!ke->sens){
+                        ke->sens = false;
+                        params++;
+                    }
+                break;
+                default:    // probaly it's possible to ignore unknows parameters
+                    fprintf(stderr, "Illegal option [%c]\n", c);
+                    return logerr(-1, "Illegal [%c], params [%d] argc %d", c, params, argc);
+            }
+    }
+    return logret(argc, "params %d, argc %d", params, argc);
+}
+
+static const char   *usage_str = "Usage: %s -v -i\n";
+
+typedef struct {
+    const char      *word;
+    int              count;
+} tkeys;
+
+static tkeys             keytab[] = {
+    {"auto"      , 0},
+    {"bool"      , 0},
+    {"break"     , 0},
+    {"case"      , 0},
+    {"char"      , 0},
+    {"const"     , 0},
+    {"continue"  , 0},
+    {"default"   , 0},
+    {"do"        , 0},
+    {"double"    , 0},
+    {"else"      , 0},
+    {"enum"      , 0},
+    {"extern"    , 0},
+    {"float"     , 0},
+    {"for"       , 0},
+    {"goto"      , 0},
+    {"if"        , 0},
+    {"int"       , 0},
+    {"long"      , 0},
+    {"register"  , 0},
+    {"return"    , 0},
+    {"short"     , 0},
+    {"signed"    , 0},
+    {"sizeof"    , 0},
+    {"static"    , 0},
+    {"struct"    , 0},
+    {"switch"    , 0},
+    {"typedef"   , 0},
+    {"union"     , 0},
+    {"unsigned"  , 0},
+    {"void"      , 0},
+    {"volatile"  , 0},
+    {"while"     , 0},
+    {0x0         , 0}
+};
+
+static int               tkeys_print(const tkeys *arr);
+
+static tkeys            *tkey_binsearch_p(fs word, tkeys *tab, int n);
+
+// probably it's better from fileutils
+static fs                getword(fs str, bool sens);
+
+int                      main(int argc, const char *argv[]){
+    logsimpleinit("Start");
+
+    Keys    ke = Keysinit();
+    argc = parse_keys(argv, &ke);
+
+    if (argc < 0) {
+        printf(usage_str, *argv);
+        return 1;
+    }
+    if (ke.version){
+        printf("%s KR find_keyword p6.3\n", __FILE__);
+        printf(usage_str, *argv);
+        return 0;
+    }
+
+    tkeys  *p;
+    FILE   *f = stdin;  // -f?
+    fs      s = FS();    // init with alloc
+    buffer_set(f);
+
+    while ( !fsisempty(s = getword(s, ke.sens) ) ) {
+            if ( (p = tkey_binsearch_p(s, keytab, COUNT(keytab) - 1) ) != 0)
+                p->count++;
+    }
+    fsfree(s);
+    printf("Total: %d\n", tkeys_print(keytab) );
+
+    return logret(0, "end...");  // as replace of logclose()
+}
+
+static tkeys             *tkey_binsearch_p(fs word, tkeys *tab, int n){
+    int         cond;
+    tkeys      *low = tab + 0;
+    tkeys      *high = tab + n;
+    tkeys      *mid;
+    const char *s = fsstr(word);
+
+    while (low < high){
+        mid = low + (high - low) / 2;
+        if ( (cond = strcmp(s, mid->word ) ) < 0)
+            high = mid;
+        else if (cond > 0)
+            low = mid + 1;
+        else
+            return mid;
+    }
+    return 0;
+}
+
+static int               tkeys_print(const tkeys *arr){
+    int     total = 0;
+    while (arr->word){
+        if (arr->count > 0){
+            printf("%4d: %s\n", arr->count, arr->word);
+            total += arr->count;
+        }
+        arr++;
+    }
+    return total;
+}
+
+static inline int conv(int c, bool sens){
+    return sens ? c: tolower(c);
+}
+
+static int               skip_cl(void){
+    int             cnt = 0;        // total count of comments
+    int             c, c1;
+    bool            comment_and_lines = true;
+
+    while (comment_and_lines){
+        while (isspace( c = getch() ) )
+            ;
+        // SKIP comment or literals
+        comment_and_lines = false;
+        if (c == '/'){
+            if ( (c1 = getch() ) == '/'){  // start comment type 1
+                cnt++;
+                comment_and_lines = true;
+                while ( (c1 = getch()) != EOF && c1 != '\n')
+                    ;
+            } else if (c1 == '*'){  // comment type 2
+                cnt++;
+                comment_and_lines = true;
+                do {
+                    while ( (c1 = getch()) != EOF && c1 != '*')
+                        ;
+                } while ( (c1 = getch() ) != '/' && c1 != EOF);  // end of /* */
+            } else  // not a comment! comment_and_lines remains false
+                c = c1; // just like ungetch
+        } else if (c == '"') {
+            comment_and_lines = true;   // line, so setup flag
+            while ( (c = getch()) != EOF && c != '"')
+                ;
+        } else if (c == '\'') {
+            comment_and_lines = true;   // anyway!
+            while ( (c = getch()) != EOF && c != '\'')
+                ;
+        }
+        //logsimple("comment_and_lines %s", bool_str(comment_and_lines));
+    }
+    return logsimpleret(c, "[%c], comments %d", c, cnt);
+}
+
+// str must have heap alloc
+static fs                getword(fs str, bool sens){
+
+    logenter("sens %s", bool_str(sens) );
+
+    fsclear(str);   // reset
+    int              c;
+    fsnew            iter = fsinew(&str);
+    c = skip_cl();
+    // HERE NORMAL TEXT
+    if (c != EOF){
+        elemnext(iter) = conv(c, sens);
+    }
+    //else        elemclear(iter);    // end flag
+
+    if (!isalpha_u(c) ){
+        elemend(iter);
+        return logret(str, "%c:%d - [%s]", c, str.len, str.v);
+    }
+    while ( (c = getch()) != EOF){
+        if (!isalnum_u(c) ){
+            ungetch(c);
+            break;
+        } else
+            elemnext(iter) = conv(c, sens);
+    }
+    elemend(iter);
+
+    return logret(str, "%d - [%s]", str.len, str.v); // that is probably new str
+}
+
