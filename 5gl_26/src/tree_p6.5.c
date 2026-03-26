afde9e78288658bb7ab8a63d1519f38388ee807f
Nick-Msk <vmksk.cur@gmail.com>
2026-03-26 14:24:58 +0300

    KR: find words via tree, p6.5 inttree_linkedfs is added for count based tree (NO_TEST)

--- 5gl_26/src/tree_p6.5.c
+++ 5gl_26/src/tree_p6.5.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "error.h"
+#include "fs.h"
 #include "tree_p6.5.h"
 
 // simple printer
@@ -35,7 +36,7 @@ static tnode            *treecreatenode(fs *str){
     return root;
 }
 
-tnode                  *treeadd(tnode *restrict root, fs *restrict str){   // fs * because have to destroy original fs
+tnode                  *treeadd(tnode *restrict root, fs *restrict str){   // fs * because have to destroy original fs (probably after using fs_move() )
     int     cond;
     if (!root)
         root = treecreatenode(str);
@@ -63,6 +64,89 @@ int                     treeprint(const tnode* node){
         cnt += treeprint(node->left);
         treeprintnode(node), cnt++;
         cnt += treeprint(node->right);
+    }
+    return cnt;
+}
+
+// -------------------------------------- Int tree linked fs ----------------------------------------------------
+
+// now only 1 element
+static int                          printfslist(const fs *str){
+    int cnt = 0;
+    if (str)    // iteration must be here
+        cnt += printf("[%s]\n", str->v );
+    return cnt;
+}
+
+// now only 1 fs
+static void                         freefslist(fs *str){
+    fs_free(str);   // only alloc will be freed
+}
+
+// free only current node
+static void                         inttreefreenode(inttree_linkedfs *root){
+    if (root->words)
+        freefslist(root->words);
+    free(root);
+}
+
+
+static inline inttree_linkedfs     *inttreealloc(void){
+    return (inttree_linkedfs *) malloc(sizeof(inttree_linkedfs));
+}
+
+static inttree_linkedfs            *inttreecreatenode(int value, const fs *str){
+    inttree_linkedfs *root = inttreealloc();
+    if (! root)
+        userraiseint(ERR_UNABLE_ALLOCATE, "%zu bytes", sizeof(inttree_linkedfs) );
+    root->value = value;
+    root->cnt = 1;
+    // TODO: ref here!!!!!
+    root->words = malloc(sizeof(fs) );
+    if (!root->words)
+        userraiseint(ERR_UNABLE_ALLOCATE, "%zu bytes", sizeof(fs) );
+    *root->words = fsclone(*str); // should be list of fs here
+    return root;
+}
+
+// -------------------- Public API initree -----------------------------------
+
+int                                 intttreeprintnode(const inttree_linkedfs* node){
+    int cnt = 0;
+    if (node){
+        cnt += printf("Value %d, sz %d, first of string: ", node->value, node->cnt);
+        cnt += printfslist(node->words);
+    }
+    return cnt;
+}
+
+void                                intttreefree(inttree_linkedfs *root){
+    if (root){
+        intttreefree(root->left);
+        intttreefree(root->right);
+        inttreefreenode(root);
+    }
+}
+inttree_linkedfs                   *inttreeadd(inttree_linkedfs *restrict root, const fs *restrict str){
+    int     cond;
+    int     value = str->len;
+    if (!root)
+        root = inttreecreatenode(value, str);
+    else if ( (cond = (value - root->value) ) == 0) // find + attach
+        root->cnt++;
+    else if (cond < 0)
+        root->left = inttreeadd(root->left, str);
+    else    // cond > 0
+        root->right = inttreeadd(root->right, str);
+    return root;
+}
+
+int                                 intttreeprint(const inttree_linkedfs* node){
+    int     cnt = 0;
+    if (node){
+        cnt += intttreeprint(node->left);
+        intttreeprintnode(node), cnt++;
+        cnt += intttreeprint(node->right);
     }
     return cnt;
 }
--- 5gl_26/src/tree_p6.5.h
+++ 5gl_26/src/tree_p6.5.h
@@ -8,15 +8,28 @@
 // ---------------------------------------------------------------------------------
 
 typedef struct tnode {
-    fs                 word;
-    int                cnt;
-    struct tnode      *left;
-    struct tnode      *right;
+    fs                      word;
+    int                     cnt;
+    struct tnode           *left;
+    struct tnode           *right;
 } tnode;
 
-extern int               treeprint(const tnode* root);
-extern tnode            *treeadd(tnode *restrict root, fs *restrict str);   // fs * because have to destroy original fs
-extern void              treefree(tnode *t);
+extern int                  treeprint(const tnode* root);
+extern tnode               *treeadd(tnode *restrict root, fs *restrict str);   // fs * because have to destroy original fs
+extern void                 treefree(tnode *t);
+
+// tree based on counts with a list of values TODO: now only COUNT of fs with 1-st fs
+typedef struct inttree_linkedfs {
+    int                         value;
+    int                         cnt; // count of list
+    fs                         *words;  // now ONLY 1 fs
+    struct inttree_linkedfs    *left;
+    struct inttree_linkedfs    *right;
+} inttree_linkedfs;
+
+extern void                 intttreefree(inttree_linkedfs *root);
+extern inttree_linkedfs    *inttreeadd(inttree_linkedfs *restrict root, const fs *restrict str);
+extern int                  intttreeprint(const inttree_linkedfs* root);
 
 #endif /* !_TREE_P6_5_H */
 
