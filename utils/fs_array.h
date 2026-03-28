6e94ce19fa40b7ace5a33493328def789b899f8b
Nick-Msk <vmksk.cur@gmail.com>
2026-03-28 12:56:13 +0300

    UTILS: fs_array: ref use be normal array with fs stage3 (NO_TEST)

--- utils/fs_array.h
+++ utils/fs_array.h
@@ -11,10 +11,8 @@
 #include <string.h>
 //
 #include "bool.h"
-#include "log.h"
-#include "common.h"
-#include "error.h"
 #include "fs.h"
+#include "log.h"
 
 // ------------------- CONSTANTS AND GLOBALS ---------------------------
 
@@ -24,29 +22,37 @@ static const int                G_FSARRAY_DEFAULT_INIT = 10;
 
 typedef struct fsarray {
     int         sz;     // total fs * allocated
-    int         cnt;    // not sure if needed
-    int         ptr;    // pointer for attach
     fs         *ar;
 } fsarray;
 
+// ------------------ API Constructs/Destrucor  ----------------------------
 // returns count of freed
-extern int              fsarr_free(fsarray *arr);
+extern int                  fsarr_free(fsarray *arr);
 
-extern fsarray          fsarr_init(int cnt);
+extern fsarray              fsarr_init(int cnt);
 
-static inline fsarray   fsarr_empty(){
+static inline fsarray       fsarr_empty(){
     return fsarr_init(G_FSARRAY_DEFAULT_INIT);
 }
 
 #define fsarrfree(s)    fsarr_free(&(s))
-#define FSARRAY(...)    (fsarray) {.sz = 0, .cnt = 0, .ptr = 0, .ar = 0, ##__VA_ARGS__};
+#define FSARRAY(...)    (fsarray) {.sz = 0, .ar = 0, ##__VA_ARGS__};
+
 // -------------------- ACCESS AND MODIFICATORS ------------------------
 
 // low level deatch
-static inline fs        fsarr_detach(fsarray *arr, int pos){
+static inline fs            fsarr_detach(fsarray *arr, int pos){
     fs s = fsmove(arr->ar[pos]);    // move via macro, fs_move undelaying
-    arr->cnt--;     // just for logging now, but arr->pos remains the same!
     return s;       // probably it's possible to not return noting
+}
+
+static inline bool          fsarr_attach(fsarray *arr, int pos, fs *s){
+    if (pos > arr->sz || pos < 0)
+        return logsimpleerr(false, "Invalid pos %d (sz %d)", pos, arr->sz);
+    if ( !fsisnull(arr->ar[pos] ) )
+        return logsimpleerr(false, "Can't move to not nullable fs");
+    arr->ar[pos] = fs_move(s);
+    return true;    // probably better to return fs, but not true/false
 }
 
 // check if position exists
@@ -56,20 +62,32 @@ static inline bool      fsarr_exists(const fsarray *arr, int pos){
 
 // return a fs
 static inline fs       *fsarr_get(const fsarray *arr, int pos){
-    return arr->ar[pos];        // even if fsnull
+    return arr->ar + pos;        // even if fsnull
 }
 
+extern int              fsarr_increase(fsarray *arr, int newsize);
+
+extern int              fsarr_shrink(fsarray *arr, int newsize);
+
+
+// FSL API HEHE: TODO:
 
 #define                 fsarrattach(arr, s) fsarr_attach(&(arr), s)
+
 // ------------------------ PRINTERS/CHECKERS --------------------------
 
-extern int              fsarr_techfprint(FILE *f, fsarray arr);
-static inline int       fsarr_techprint(fsarray arr){
+extern int              fsarr_fprint(FILE *restrict f, const fsarray *restrict arr);
+static inline int       fsarr_print(const fsarray* arr){
+    return fsarr_fprint(stdout, arr);
+}
+
+extern int              fsarr_techfprint(FILE *restrict f, const fsarray *restrict arr);
+static inline int       fsarr_techprint(const fsarray *arr){
     return fsarr_techfprint(stdout, arr);
 }
 
-extern int              fsarr_techfprintlim(FILE *f, fsarray arr, int lim);
-static inline int       fsarr_techprintlim(fsarray arr, int lim){
+extern int              fsarr_techfprintlim(FILE *restrict f, const fsarray *restrict arr, int lim);
+static inline int       fsarr_techprintlim(const fsarray *arr, int lim){
     return fsarr_techfprintlim(stdout, arr, lim);
 }
 
--- utils/src/fs_array.c
+++ utils/src/fs_array.c
@@ -5,6 +5,7 @@
 #include "fs.h"
 #include "error.h"
 #include "fs_array.h"
+#include "fileutils.h"
 
 /********************************************************************
                     FAST STRING ARRAY MODULE IMPLEMENTATION
@@ -14,30 +15,38 @@
 
 // -------------------------- (Utility) printers --------------------
 
-int                             fsarr_techfprintlim(FILE *f, fsarray arr, int lim){
-    int cnt = fprintf(f, "FSARRAY: sz %d, cnt %d, ptr %d namedfs ptr %p:\n", arr.sz, arr.cnt, arr.ptr, arr.ar);
-
-    if (arr.ar)
-        for (int i = 0; i < MIN(arr.sz, lim); i++){
-            if (arr.ar[i].ps) {
-                cnt += fstechfprint(f, *arr.ar[i]);   // directly name!
+int                             fsarr_techfprintlim(FILE *restrict f, const fsarray *restrict arr, int lim){
+    int cnt = 0;
+    if (arr){
+        cnt += fprintf(f, "FSARRAY: sz %d, ptr %p:\n", arr->sz, arr->ar);
+        if (arr->ar)
+            for (int i = 0; i < MIN(arr->sz, lim); i++){
+                cnt += fstechfprint(f, arr->ar[i]);   // directly name!
             }
-        }
-    if (lim < arr.sz)
-        cnt += fprintf(f, "and more... (%d)\n", arr.sz - lim);
+        if (lim < arr->sz)
+            cnt += fprintf(f, "and more... (%d)\n", arr->sz - lim);
+    }
     return cnt;
 }
 
-int                             fsarr_techfprint(FILE *f, fsarray arr){
-    return fsarr_techfprintlim(f, arr, arr.sz);
+int                             fsarr_techfprint(FILE *restrict f, const fsarray *restrict arr){
+    return fsarr_techfprintlim(f, arr, arr->sz);
 }
 
 // ------------------------------ Utilities -------------------------
 
-// should depent on increase strategy, simplest return n + 1;
-static inline int               calcnewsize(int n){
-    int sz = round_up_2(n);
-    return sz; //logsimpleret(sz, "newsz = %d", sz);
+static int                      fsarr_mass_init(fsarray *arr, int min, int max){
+    for (int i = min; i < max; i++)
+        arr->ar[i] = FS();  // with alloc flag
+    return min <= max ? max - min : 0;
+}
+
+static int                      fsarr_mass_free(fsarray *arr, int min, int  max){
+    int cnt = 0;
+    for (int i = max - 1; i >= min; i--)
+        if (!fsisnull(arr->ar[i]) )
+            fsfree(arr->ar[i]), cnt++;
+    return cnt;
 }
 
 static int                      increasesize(fsarray *fa, int newsz, bool init){
@@ -45,13 +54,152 @@ static int                      increasesize(fsarray *fa, int newsz, bool init){
     if (init)   // from fsarr_init
         newsz = calcnewsize(SIZE_POWER2, newsz);
     if (newsz > fa->sz || !init) {   // if exec from faarr_increate() then resize anyway
-        namedfs *tmp = realloc(fa->ar, newsz * sizeof (namedfs) );
+        fs *tmp = realloc(fa->ar, newsz * sizeof (fs) );
         if (!tmp) {
-            return userraise(-1, 10, "Unable to allocate %lu bytes", newsz * sizeof (namedfs) );  // what about LG_LV here??/ TODO:
+            return userraise(-1, 10, "Unable to allocate %lu bytes", newsz * sizeof (fs) );  // what about LG_LV here??/ TODO:
         }
         fa->sz = newsz;
         fa->ar = tmp;
     }
     return logret(newsz, "Increased to %d elements", newsz);
 }
+
+// --------------------------- API ---------------------------------
+// ------------------ General functions ----------------------------
+
+// -------------------- ACCESS AND MODIFICATORS ------------------------
+
+// TODO: via fsl ( struct fsl { int loc; }; 
+// fsl f1 = getfsl(fs_array a);
+// f1.elem() = ... 
+// f1.get() = ...
+
+int                         fsarr_increase(fsarray *arr, int newsize){
+    int sz = arr->sz;
+    increasesize(arr, newsize, false);
+    fsarr_mass_init(arr, sz, newsize);
+    return logsimpleret(arr->sz, "New sz %d", arr->sz);
+}
+
+int                         fsarr_shrink(fsarray *arr, int newsize){
+    if (newsize < arr->sz && newsize >= 0){
+        fsarr_mass_free(arr, newsize, arr->sz);
+        increasesize(arr, newsize, false);
+    }
+    return logsimpleret(arr->sz, "Shrinked to %d", arr->sz);
+}
+
+// -------------------------- (API) printers -----------------------
+
+// this is normal printer
+int                         fsarr_fprint(FILE *restrict f, const fsarray *restrict arr){
+    int  cnt = 0;
+    for (int i = 0; i < arr->sz; i++){
+        if ( !fsisnull(arr->ar[i] ) ) {
+            fsfprint(f, arr->ar[i]);   // directly name!
+            cnt++;
+            fputc('\n', f);
+        }
+    }
+    return cnt;     // total printed items
+}
+
+// -------------------------- (API) Serialization  -----------------------
+int                         fsarr_save(const char *restrict fname, const fsarray *restrict arr){
+    FILE    *out = fopen(fname, "w");
+    if (!out)
+        return userraise(ERR_UNABLE_OPEN_FILE_READ, -1, "Unable to open %s for write", fname);
+    int     cnt = fsarr_savef(out, arr);
+    fclose(out);
+    return cnt;
+}
+
+// f is open for write
+int                         fsarr_fsave(FILE *restrict f, const fsarray *restrict arr){
+    // signature
+    fprintf(f, "FSARRAY(%d):[\n", arr->sz);
+    if (!arr->ar)
+        //return logsimpleret(-1, "Nullable ar, can't serialize");    // userraise?
+        return userraise(ERR_NULLABLE_PTR, -1, "Nullable ar, can't serialize"); 
+    for (int i = 0; i < arr->sz; i++){
+        fprintf(f, "%4d:", i);
+        fs_fsave(f, fsarr_get(arr, i) );
+        fputc('\n', f);
+    }
+    fprintf(f, "]");
+    return logsimpleret(arr->sz, "Total saved %d", arr->sz);
+}
+
+fsarray                     fsarr_load(const char *fname){
+    FILE    *out = fopen(fname, "r");
+    if (!out)
+        userraiseint(ERR_UNABLE_OPEN_FILE_READ, "Unable to open %s for read", fname);
+    fsarray fa = fsarr_loadf(out);
+    fclose(out);
+    return fa;
+}
+
+fsarray                     fsarr_fload(FILE *restrict f){
+    // format: "FSARRAY: sz %d cnt %d ptr %d:[\n"
+    FUSKIPFORMAT(f, "FSARRAY(");
+    int cnt = FUGETUNSIGNED(f);
+
+    FUSKIPFORMAT(f, "):[\n");
+    fsarray     a = fsarr_init(cnt);
+    // cycle for fs
+    for (int i = 0; i < cnt; i++){
+        a.ar[i] = fs_fload(f, 0);       // TODO: probably to usr fsarr_get()?
+    }
+    FUSKIPFORMAT(f, "]");
+    return a;
+}
+
+// ------------------ API Constructs/Destrucor  ----------------------------
+
+int                 fsarr_free(fsarray *arr){
+
+    logenter("%p: %p, %d", arr, arr ? arr->ar : 0, arr ? arr->sz : 0);
+    int   cnt = 0;
+    if (arr)
+        cnt = fsarr_mass_free(arr, 0, arr->sz);
+    return logret(cnt, "Freed %d", cnt);
+}
+
+// initialize
+fsarray             fsarr_init(int sz){
+    fsarray ar = FSARRAY();
+    increasesize(&ar, sz, true);
+    fsarr_mass_init(&ar, 0, ar.sz);
+    return logsimpleret(ar, "Created with sz %d", ar.sz);
+}
+
+// -------------------------------Testing --------------------------
+#ifdef FSARRAYTESTING
+
+#include "test.h"
+#include "checker.h"
+
+//types for testing
+
+// ------------------------- TEST 1 ---------------------------------
+
+// ------------------------------------------------------------------
+int
+main( /* int argc, const char *argv[] */)
+{
+    logsimpleinit("Start");
+
+    testenginestd(
+        testnew(.f2 = tf1, .num = 1, .name = "Simple init free test"                        , .desc=""                , .mandatory=true)
+     /* , testnew(.f2 = tf2, .num = 2, .name = "Init/free test with valid fs (random)"        , .desc=""                , .mandatory=true)
+      , testnew(.f2 = tf3, .num = 3, .name = "fsarr_attach test"                            , .desc=""                , .mandatory=true)
+      , testnew(.f2 = tf4, .num = 4, .name = "fsarr_save test"                              , .desc=""                , .mandatory=true)
+      , testnew(.f2 = tf5, .num = 5, .name = "fsarr_save/load test"                         , .desc=""                , .mandatory=true) */
+    );
+
+    return logcloseret(0, "end...");
+}
+
+#endif /* FSARRAYTESTING */
+
 
