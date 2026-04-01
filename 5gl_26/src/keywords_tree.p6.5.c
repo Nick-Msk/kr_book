865738966d3b6708beed07101d3bc2488cd16cba
Nick-Msk <vmksk.cur@gmail.com>
2026-04-01 13:17:09 +0300

    KR: find words via tree, p6.5, refactored to use separate getword

--- 5gl_26/src/keywords_tree.p6.5.c
+++ 5gl_26/src/keywords_tree.p6.5.c
@@ -10,6 +10,7 @@
 #include "error.h"
 #include "buffer.h"
 #include "tree_p6.5.h"
+#include "getword.h"
 
 typedef struct Keys {
     bool    version;
@@ -48,8 +49,6 @@ static int              parse_keys(const char *argv[], Keys *ke){
     return logret(argc, "params %d, argc %d", params, argc);
 
 }
-
-static fs                getword(fs str, bool sens);
 
 static const char   *usage_str = "Usage: %s -v -i\n";
 
@@ -108,84 +107,5 @@ int                      main(int argc, const char *argv[]){
         logmsg("Warning: incorrect allocation of fs's");
 
     return logret(0, "end...");  // as replace of logclose()
-}
-
-static inline int       skip_spaces(void){
-    int c;
-    while (isspace( c = getch() ) )
-        ;
-    return c;
-}
-
-/*
-static int               skip_cl(void){
-    int             cnt = 0;        // total count of comments
-    int             c, c1;
-    bool            comment_and_lines = true;
-
-    while (comment_and_lines){
-
-        c = skip_spaces();
-        // SKIP comment or literals
-        comment_and_lines = false;
-        if (c == '/'){
-            if ( (c1 = getch() ) == '/'){  // start comment type 1
-                cnt++;
-                comment_and_lines = true;
-                while ( (c1 = getch()) != EOF && c1 != '\n')
-                    ;
-            } else if (c1 == '*'){  // comment type 2
-                cnt++;
-                comment_and_lines = true;
-                do {
-                    while ( (c1 = getch()) != EOF && c1 != '*')
-                        ;
-                } while ( (c1 = getch() ) != '/' && c1 != EOF);  // end of /* */ /*
-            } else  // not a comment! comment_and_lines remains false
-                c = c1; // just like ungetch
-        } else if (c == '"') {
-            comment_and_lines = true;   // line, so setup flag
-            while ( (c = getch()) != EOF && c != '"')
-                ;
-        } else if (c == '\'') {
-            comment_and_lines = true;   // anyway!
-            while ( (c = getch()) != EOF && c != '\'')
-                ;
-        }
-        logsimple("comment_and_lines %s", bool_str(comment_and_lines));
-    }
-    return logsimpleret(c, "[%c], comments %d", c, cnt);
-}*/
-
-
-// str must have heap alloc
-static fs                getword(fs str, bool sens){
-
-    logenter("sens %s", bool_str(sens) );
-
-    fsclear(str);   // reset
-    int              c;
-    fsnew            iter = fsinew(&str);
-
-    c = skip_spaces();       // comment and so on are allowed! TODO: probably use flag -c
-    if (c != EOF){
-        elemnext(iter) = clower(c, sens);
-    } else
-        elemclear(iter);    // end flag
-
-    if (!isalpha_u(c) ){
-        elemend(iter);
-        return logret(str, "%c:%d - [%s]", c, str.len, str.v);
-    }
-    while ( (c = getch()) != EOF){
-        if (!isalnum_u(c) ){
-            ungetch(c);
-            break;
-        } else
-            elemnext(iter) = clower(c, sens);
-    }
-    elemend(iter);
-
-    return logret(str, "%d - [%s]", str.len, str.v); // that is probably new str
 }
 
