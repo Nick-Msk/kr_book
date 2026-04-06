599482d50344213a558551dfb3c813c067ceba9d
Nick-Msk <vmksk.cur@gmail.com>
2026-04-06 13:36:45 +0300

    UTILS: numeric_ops.h: initial

---
+++ utils/numeric_ops.h
@@ -0,0 +1,35 @@
+#ifndef _NUMERIC_OPS_H
+#define _NUMERIC_OPS_H
+
+/***************************************************************
+                    USEFUL NUMERIC MACRO AND FUNCTIONS
+***************************************************************/
+
+
+static inline int           isprime(int n) // assuming n > 1
+{
+    int i, root;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    root = (int) sqrt(n + 0.0);
+    for (i = 5; i <= root; i += 6)
+    {
+        if (n % i == 0)
+           return false;
+    }
+    for (i = 7; i <= root; i += 6)
+    {
+        if (n % i == 0)
+           return 0;
+    }
+    return 1;
+}
+
+static inline int           calc_next_prime(int num){
+    while (!isprime(num) )
+        num++;
+    return num;
+}
+
+#endif /* !_NUMERIC_OPS_H */
+
