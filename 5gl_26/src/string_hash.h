30aafc08758665fee5fa696ffcc196205199ba3b
Nick-Msk <vmksk.cur@gmail.com>
2026-04-08 16:09:33 +0300

    KR: string hash 6.5 upd7 fs port for string_hash

--- 5gl_26/src/string_hash.h
+++ 5gl_26/src/string_hash.h
@@ -1,6 +1,8 @@
 #ifndef _STRING_HASH_H
 #define _STRING_HASH_H
 
+#include "fs.h"
+
 // --------------------------------------------------------------------------------------
 // ----------------------------- Public fs Hash API -------------------------------------
 // --------------------------------------------------------------------------------------
@@ -29,10 +31,22 @@ extern void                strhash_free(stringhash *hashtab);
 extern unsigned            strhash(const stringhash *restrict hashtab, const char *restrict str);
 
 extern stringlist         *strhash_lookup(const stringhash *restrict hashtab, const char *restrict str);
+// fs port
+static inline stringlist  *strhash_fslookup(const stringhash *restrict hashtab, const fs *restrict str){
+    return strhash_lookup(hashtab, str->v);
+}
 
 extern stringlist         *strhash_install(stringhash *restrict hashtab,  const char *restrict str, const char *restrict val);
+// fs port
+static inline stringlist  *strhash_fsinstall(stringhash *restrict hashtab,  const fs *restrict str, const fs *restrict val){
+    return strhash_install(hashtab, str->v, val->v);
+}
 
 extern bool                strhash_undef(stringhash *restrict hashtab, const char *restrict str);
+// fs port
+static inline bool         strhash_fsundef(stringhash *restrict hashtab, const fs *restrict str){
+    return strhash_undef(hashtab, str->v);
+}
 
 extern int                 strhash_fprint(FILE *restrict out, const stringhash *restrict hashtab);
 static inline int          strhash_print(const stringhash *hashtab){
