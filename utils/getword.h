8fb22e07e408853ad06079d97b111e9c89f9c277
Nick-Msk <vmksk.cur@gmail.com>
2026-04-09 22:19:50 +0300

    UTILS: getword: minor fix

---
+++ utils/getword.h
@@ -0,0 +1,63 @@
+#ifndef _GETWORD_H
+#define _GETWORD_H
+
+#include "fs.h"
+#include "bool.h"
+#include "checker.h"
+
+extern fs                   getword(fs str, bool sens, bool comments, bool get_newline);
+// STR MUST BE init with alloc flag
+static inline bool                 getsimpleword(fs *str){
+    *str = getword(*str, false, false, false);
+    return !fsisnull(*str);
+}
+
+// LEXEM_CMD for \<cmd>
+typedef enum { LEXEM_UNK = -1, LEXEM_WORD, LEXEM_INT, LEXEM_FLOAT, LEXEM_SYM, LEXEM_CMD } Lexemtype;
+
+static inline const char   *Lexemtype_str(Lexemtype typ){
+    switch (typ){
+        CASE_RETURN(LEXEM_UNK);
+        CASE_RETURN(LEXEM_WORD);
+        CASE_RETURN(LEXEM_INT);
+        CASE_RETURN(LEXEM_FLOAT);
+        CASE_RETURN(LEXEM_SYM);
+        CASE_RETURN(LEXEM_CMD);
+        default: return "";
+    }
+}
+
+typedef struct Lexem {
+    fs          str;
+    Lexemtype   typ;
+} Lexem;
+
+static inline const char *  Lexemstr(Lexem l){
+    return fsstr(l.str);
+}
+
+static inline void                 Lexem_free(Lexem *l){
+    invraise(l != 0, "Wrong input");
+    fsfree(l->str);
+    l->typ = LEXEM_UNK;
+}
+
+#define                     Lexeminit(...) {.str = FS(), .typ = LEXEM_UNK }
+#define                     Lexemfree(l) Lexem_free( &(l) )
+
+// any lexem, word or number
+extern bool                 getlexem(Lexem *lex, bool ign_comments);
+
+// invariant: lex != 0 && str != 0
+static inline bool          lexem_eq(const Lexem *restrict lex, const char *restrict str, int len){
+    invraise(lex != 0 && str != 0, "Wrong input");
+    return strncmp(lex->str.v, str, len);
+}
+// fs port
+static inline bool          lexem_fseq(const Lexem *restrict lex, const fs *restrict str, int len){
+    invraise(lex != 0 && str != 0, "Wrong input");
+    return fs_ncmp(&lex->str, str, len);
+}
+
+#endif /* ! _GETWORD_H */
+
