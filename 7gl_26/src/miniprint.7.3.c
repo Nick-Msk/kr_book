5d38358e97de8ddc7f9db9c22e94d4c031c858a7
Nick-Msk <vmksk.cur@gmail.com>
2026-04-11 20:36:21 +0300

    KR: miniprint task 7.3 init

---
+++ 7gl_26/src/miniprint.7.3.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "log.h"
+#include "common.h"
+#include "error.h"
+
+typedef struct Keys {
+    bool        version;    // bool example
+    bool        string;
+    char       *filename;
+    int         maxline;
+    // ...
+} Keys;
+
+#define                 Keysinit(...) (Keys){ .version = false, .string = 0, maxline = 0, __VA_ARGS__}
+
+static int              parse_keys(const char *argv[], Keys *ke){
+    logenter("...");
+    int     argc = 1, params = 0;
+    if (!ke)
+        return userraiseint(ERR_WRONG_INPUT_PARAMETERS, "Zero ke!!! Error!");   // raise here
+    char    c;
+    while (*++argv != 0 && **argv == '-'){
+        argc++;
+        while ( (c = *++argv[0]) )
+            switch (tolower(c)){
+                case 'v':
+                    if (!ke->version){
+                        ke->version = true;
+                        params++;
+                    }
+                break;
+                case 'f':
+                    // TODO: refactor that to make more pretty
+                    if (argv[0][1] == '\0') {
+                        if (argv[1]){
+                            ptr = argv[1];
+                            argv++;
+                        } else
+                             return userraise(-1, ERR_WRONG_PARAMETER, "-l option without value (must be integer followed), ex '-l123' or '-l 123'");
+                    } else  // argv[1] + 1 is pointer to value
+                        ptr = argv[0] + 1;
+                    ke->string = ptr;        // save pointer
+                    argv[0] += strlen(argv[0]) - 1; // shift
+                    params++;
+                break;
+                case 'l':
+                    // TODO: refactor that to make more pretty
+                    if (argv[0][1] == '\0') {
+                        if (argv[1]){
+                            ptr = argv[1];
+                            argv++;
+                        } else
+                             return userraise(-1, ERR_WRONG_PARAMETER, "-l option without value (must be integer followed), ex '-l123' or '-l 123'");
+                    } else  // argv[1] + 1 is pointer to value
+                        ptr = argv[0] + 1;
+                    ke->maxline = atoi(ptr);        // save pointer
+                    logauto(ptr);
+                    argv[0] += strlen(argv[0]) - 1; // shift
+                    params++;
+                break;
+                default:    // probaly it's possible to ignore unknows parameters
+                    fprintf(stderr, "Illegal option [%c]\n", c);
+                    return logerr(-1, "Illegal [%c], params [%d] argc %d", c, params, argc);
+            }
+    }
+    return logret(argc, "params %d, argc %d", params, argc);
+}
+
+const char *usage_str = "Usage: %s\n";
+
+static int              miniprint(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
+ 
+int                     main(int argc, const char *argv[]){
+    logsimpleinit("Start");
+
+    Keys    ke = Keysinit();
+    argc = parse_keys(argv, &ke);
+
+    if (argc < 0) {
+        printf(usage_str, *argv);
+        return 1;
+    }
+    if (ke.version){
+        printf("%s miniprint 7.3\n", __FILE__);
+        printf(usage_str, *argv);
+        return 0;
+    }
+
+    double d = 1.6; int i = 9; long l = 18L;
+    const char *str = "Abcde1234";
+    miniprint("Test string %s %d %f %ld\n", str, i, d, l);
+
+    return logret(0, "end...");  // as replace of logclose()
+}
+
+static int              miniprint(const char *fmt, ...) __attribute__ ((format (printf, 1, 2))){
+    
+}
