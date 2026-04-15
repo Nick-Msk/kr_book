7941882dfbea01c5783a61b6a8f0019d48312057
Nick-Msk <vmksk.cur@gmail.com>
2026-04-15 21:05:19 +0300

    KR: p7.5 pg 174 minor adjustmemnt

--- 7gl_26/src/cat.p7.5.c
+++ 7gl_26/src/cat.p7.5.c
@@ -13,18 +13,21 @@ int                     main(int argc, const char *argv[]){
     logsimpleinit("Start");
 
     FILE    *fp;
+    const char *prog = *argv;
 
     if (argc == 1)
         filecopy(stdout, stdin);
     else
         while (--argc > 0){
             if ( (fp = fopen(*++argv, "r") ) == 0)
-                userraiseint(ERR_UNABLE_OPEN_FILE_READ, "cat: unable to open file %s\n", *argv);
+                userraiseint(ERR_UNABLE_OPEN_FILE_READ, "%s: unable to open file %s\n", prog, *argv);
             else {
                 filecopy(stdout, fp);
                 fclose(fp);
             }
         }
+    if (ferror(stdout))
+        userraiseint(ERR_STREAM_ERROR, "%s: error while writing to stdout\n", prog);
     return logret(0, "end...");  // as replace of logclose()
 }
 
