544334dc2d64b57d687463741beff3139214a37e
Nick-Msk <vmksk.cur@gmail.com>
2026-04-15 21:02:02 +0300

    UTILS: common.h: ERR_STREAM_ERROR is added

--- utils/error.h
+++ utils/error.h
@@ -30,6 +30,7 @@ static const int                        ERR_WRONG_PARAMETER         = 22;
 static const int                        ERR_UNABLE_OPEN_FILE        = 50;
 static const int                        ERR_UNABLE_OPEN_FILE_READ   = 51;
 static const int                        ERR_UNABLE_OPEN_FILE_WRITE  = 52;
+static const int                        ERR_STREAM_ERROR            = 53;
 
 static const int                        ERR_WRONG_INPUT_PARAMETERS  = 60;
 
