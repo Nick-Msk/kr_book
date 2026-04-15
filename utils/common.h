b6b3ba870366b68d5a873f577a8a8f3d93fb821e
Nick-Msk <vmksk.cur@gmail.com>
2026-04-15 18:05:43 +0300

    UTILS: common.h: singleton and modexec(s) are added

--- utils/common.h
+++ utils/common.h
@@ -266,10 +266,10 @@ static inline int cupper(int c, bool upper){
     return upper ? toupper(c) : c;
 }
 
-typedef enum {SIZE_NONE = 0, SIZE_POWER2, SIZE_MIN10 } Tincrease; 
+typedef enum {SIZE_NONE = 0, SIZE_POWER2, SIZE_MIN10 } Tincrease;
 
 // should depent on increase strategy, simplest return n + 1;
-static inline int               calcnewsize(Tincrease t, int n){ 
+static inline int               calcnewsize(Tincrease t, int n){
     int sz = n;
     switch (t){
         case SIZE_NONE:
@@ -307,5 +307,31 @@ static inline bool              int__in(int val, const int *arr){
 // TODO: think about typeof
 #define                         int_notin(val, ...) int_not_in( (val),  (const int []) { __VA_ARGS__, INT_MIN} )
 #define                         int_in(val, ...)    int__in( (val),     (const int []) { __VA_ARGS__, INT_MIN} )
+
+#define    SINGLETON(action)\
+        ({ static bool _SINGLE_11 = true;\
+            bool tmp = _SINGLE_11;\
+          if (_SINGLE_11){\
+                (action);\
+                _SINGLE_11 = false;\
+          }\
+          tmp;\
+        })
+
+#define   MODEXEC(modval, action)\
+        ({ static unsigned _MODVAL = 0;\
+           bool res = (++_MODVAL % modval == 0);\
+           if (res)\
+               (action);\
+            res;\
+        })
+
+#define   MODEXECL(modval, action)\
+        ({ static unsigned long _MODVAL = 0;\
+           bool res = (++_MODVAL % modval == 0);\
+           if (res)\
+               (action);\
+            res;\
+        })
 
 #endif /* ! _COMMON_H */
