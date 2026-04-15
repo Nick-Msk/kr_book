bcb12650e09fc4714e02d73acd7b4df10470556e
Nick-Msk <vmksk.cur@gmail.com>
2026-04-15 20:59:25 +0300

    KR: p7.5 pg 174 (TEST_OK)

--- 7gl_26/src/cat.p7.5.c
+++ 7gl_26/src/cat.p7.5.c
@@ -7,11 +7,32 @@
 
 const char *usage_str = "Usage: %s <val1:TYPE1> <val2:TYPE2>\n";
 
+static int              filecopy(FILE *to, FILE *from);
+
 int                     main(int argc, const char *argv[]){
     logsimpleinit("Start");
 
-    
+    FILE    *fp;
 
+    if (argc == 1)
+        filecopy(stdout, stdin);
+    else
+        while (--argc > 0){
+            if ( (fp = fopen(*++argv, "r") ) == 0)
+                userraiseint(ERR_UNABLE_OPEN_FILE_READ, "cat: unable to open file %s\n", *argv);
+            else {
+                filecopy(stdout, fp);
+                fclose(fp);
+            }
+        }
     return logret(0, "end...");  // as replace of logclose()
 }
+
+static int              filecopy(FILE *to, FILE *from){
+    int     c, cnt = 0;
+    while ( (c = getc(from) ) != EOF)
+        putc(c, to), cnt++;
+    return cnt;
+}
+
 
