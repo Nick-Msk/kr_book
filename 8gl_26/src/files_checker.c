004e6042e26c43c789652bf015f3c32259bac11e
Nick-Msk <vmksk.cur@gmail.com>
2026-04-21 17:56:18 +0300

    KR: p8.5 pg187-90 upd5 (NO_TEST)

--- 8gl_26/src/files_checker.c
+++ 8gl_26/src/files_checker.c
@@ -6,6 +6,7 @@
 #include "error.h"
 #include "getword.h"
 #include "files.p8.5.h"
+#include "fs.h"
 
 const char                 *usage_str = "Usage: %s -v -f\n";
 
@@ -81,11 +82,87 @@ int     main(int argc, const char *argv[]){
     return logret(res, "end...");  // as replace of logclose()
 }
 
+typedef struct {
+    Lexem       *lex;
+    MFILE       *mfr;   // read
+    MFILE       *mfw;   // write
+    bool        quit;   // flag for quit
+    // ....
+} Context;
+#define             ContextInit(...) (Context) {.lex = 0, .mfr = 0, .mfw = 0, .quit = false,  ##__VA_ARGS__}
+
+typedef int         (*process_unit)(Context *c);   // function, process context
+
+typedef struct Command {
+    const char *    name;
+    process_unit    proc;
+} Command;
+
+#define             CommandInit(...) (Command) {.name = 0, .proc = 0, ##__VA_ARGS__}
+
+// example of working procedures
+int                 proc_quit(Context *ctx){
+    ctx->quit = true;
+    return 1;
+}
+
+int                 proc_create(Context *ctx){
+    Lexem *l = ctx->lex;
+    if (getlexem(l, false) )
+        if (l->typ == LEXEM_WORD){
+            const char *fname = fsstr(l->str);
+            MFILE      *mf;
+            if ( (mf = mopen(fname, "w") ) == 0)
+                fprintf(stderr, "Unable to create %s\n", fname);
+            else {
+                printf("File %s is opened for write\n", fname);
+                ctx->mfw = mf;       // setup ctx;
+                return logsimpleret(1, "File %s was created and opened for write", fname);
+            }
+        } else
+            fprintf(stderr, "Word expected but (%s)", Lexemtype_str(ctx->lex->typ) );
+    else
+        fprintf(stderr, "Out of input\n");
+    return logsimpleerr(-1, "Failed to create");  // failed
+}
+
+static Command cmds[] = {
+    CommandInit(.name = "quit"      , .proc = proc_quit )
+  , CommandInit(.name = "create"    , .proc = proc_create )
+  , CommandInit(.name = "!!!END!!!" , .proc = 0 )
+};
+
+// WILL BE MOVED Out TO command_executor, so can't usr cmds directly
+// go through cmd until cmd->proc is null
+static bool         process_command(const char *restrict name, Command *restrict cmd, Context *restrict ctx){
+    logenter("Cmd [%s]", name);
+    // 1-st version, just a fullscan
+    while (cmd->proc){
+        if (strcmp(name, cmd->name ) == 0){
+            cmd->proc(ctx);
+            return logret(true, "Command %s was processed", name);
+        }
+        cmd++;      // next
+    }
+    return logerr(false, "Command %s not found", name);
+}
+
 static bool         start_checking(const char *filename){
     logenter("%s", filename);
 
     bool        ret = true;
 
+    Lexem       lex = lexeminit();
+    Context     ctx = ContextInit();
+    printf("Start with (%s)>", filename);
+
+    while (!ctx.quit && getlexem(&lex, false) ){
+        if (lex.typ == LEXEM_CMD){
+            // find + exec
+            process_command( fsstr(lex.str), cmds, &ctx);
+        } else
+            fprintf(stderr, "Incorrent lexem type %d:%s", lex.typ, Lexemtype_str(lex.typ) );
+    }
     return logret(ret, "%s", bool_str(ret) );
 }
 
