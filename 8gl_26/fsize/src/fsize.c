045b934ae176aca698c951b5a76d90fbbaf8f108
Nick-Msk <vmksk.cur@gmail.com>
2026-04-25 21:22:11 +0300

    KR: fsize p8.6 p191 upd1 (TEST_OK)

--- 8gl_26/fsize/src/fsize.c
+++ 8gl_26/fsize/src/fsize.c
@@ -8,16 +8,16 @@
 #include "checker.h"
 //#include "fs.h"
 
+static const int        MAX_PATH = 8192;
+
 typedef int             (*tfcn)(const char *);
 
 static int              dirwalk(const char *dir, tfcn func);
 
-static TDIR            *opentdir(const char *dir);
-static TDirent         *readtdir(TDIR *);
-static void             closetdir(TDIR *);
-
 extern int              fsize(const char *name){
     invraise(name != 0, "NUll input");
+
+    logsimple("Filename %s", name);
     struct stat stbuf;
     int         cnt = 0;
 
@@ -31,26 +31,27 @@ extern int              fsize(const char *name){
 }
 
 static int              dirwalk(const char *dir, tfcn func){
+    logsimple("dir %s", dir);
     //fs                 name = FS();
     int                cnt = 0;
     char               name[MAX_PATH];
-    TDirent           *dp;
-    TDIR              *dfd;
+    struct dirent     *dp;
+    DIR               *dfd;
 
-    if ( (dfd = opentdir(dir)) == 0)
+    if ( (dfd = opendir(dir)) == 0)
         return sysraise(0, "%s: can't open %s", __func__, dir);
-    while ( (dp = readtdir(dfd) ) != 0){
-        if ( (strcmp(dp->name, ".") == 0) ||
-            (strcmp(dp->name, "..") == 0) )
+    while ( (dp = readdir(dfd) ) != 0){
+        if ( (strcmp(dp->d_name, ".") == 0) ||
+            (strcmp(dp->d_name, "..") == 0) )
         continue;
-        if (strlen(dir) + strlen(dp->name) + 2 >= sizeof(name))
-            return userraise(0, ERR_TOO_LONG_LINE, "%s: name %s %s too long", __func__, dir, dp->name);
+        if (strlen(dir) + strlen(dp->d_name) + 2 >= sizeof(name))
+            return userraise(0, ERR_TOO_LONG_LINE, "%s: name %s %s too long", __func__, dir, dp->d_name);
         else {
-            snprintf(name, sizeof(name), "%s/%s", dir, dp->name);
+            snprintf(name, sizeof(name), "%s/%s", dir, dp->d_name);
             cnt += func(name);
         }
     }
-    closetdir(dfd);
+    closedir(dfd);
     return cnt;
 }
 
