d0201f20a7088623e13705225242c90bf170b971
Nick-Msk <vmksk.cur@gmail.com>
2026-04-25 10:04:10 +0300

    TECH: skeleton2 ref to use new parse_keys.h

--- skeleton2.c
+++ skeleton2.c
@@ -5,6 +5,7 @@
 #include "common.h"
 #include "error.h"
 #include "checker.h"
+#include "parse_keys.h"
 
 typedef struct Keys {
     bool              version;    // bool example
@@ -15,57 +16,27 @@ typedef struct Keys {
 } Keys;
 
 #define                 Keysinit(...) (Keys){ .version = false, .string = 0, .maxline = 0, .filename __VA_ARGS__}
-
 static int              parse_keys(const char *argv[], Keys *ke){
 
     invraise(ke, "Zero ke!!! Error!"); // raise here
-
     logenter("...");
 
     int     argc = 1, params = 0;
     char    c;
+    const char *ptr;
+
     while (*++argv != 0 && **argv == '-'){
         argc++;
         while ( (c = *++argv[0]) )
             switch (tolower(c)){
-                case 'v':
-                     ke->version = true;
-                     params++;
-                break;
-                case 'f':
-                    // TODO: refactor that to make more pretty
-                    if (argv[0][1] == '\0') {
-                        if (argv[1]){
-                            ptr = argv[1];
-                            argv++;
-                        } else
-                             return userraise(-1, ERR_WRONG_PARAMETER, "-l option without value (must be integer followed), ex '-l123' or '-l 123'");
-                    } else  // argv[1] + 1 is pointer to value
-                        ptr = argv[0] + 1;
-                    ke->string = ptr;        // save pointer
-                    argv[0] += strlen(argv[0]) - 1; // shift
-                    params++;
-                break;
-                case 'l':
-                    // TODO: refactor that to make more pretty
-                    if (argv[0][1] == '\0') {
-                        if (argv[1]){
-                            ptr = argv[1];
-                            argv++;
-                        } else
-                             return userraise(-1, ERR_WRONG_PARAMETER, "-l option without value (must be integer followed), ex '-l123' or '-l 123'");
-                    } else  // argv[1] + 1 is pointer to value
-                        ptr = argv[0] + 1;
-                    ke->maxline = atoi(ptr);        // save pointer
-                    logauto(ptr);
-                    argv[0] += strlen(argv[0]) - 1; // shift
-                    params++;
-                break;
+                parse_bool('v', version);
+                parse_bool('s', string);
+                parse_string('f', filename);
                 default:    // probaly it's possible to ignore unknows parameters
                     return userraise(-1, ERR_WRONG_PARAMETER, "Illegal [%c], params [%d] argc %d", c, params, argc);
             }
     }
-    return logret(argc, "params %d, argc %d", params, argc);
+    return logret(argc, "params %d, argc %d, %s", params, argc, ptr = 0);
 }
 
 const char *usage_str = "Usage: %s <val1:TYPE1> <val2:TYPE2>\n";
