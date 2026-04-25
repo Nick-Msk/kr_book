7d2d0cf3c7a8f32a71786c6dc0ed36c20265ed36
Nick-Msk <vmksk.cur@gmail.com>
2026-04-25 13:28:11 +0300

    UTILS: error.h: ERR_CANT_GET_STAT is added

--- utils/error.h
+++ utils/error.h
@@ -31,7 +31,8 @@ static const int                        ERR_UNABLE_OPEN_FILE        = 50;
 static const int                        ERR_UNABLE_OPEN_FILE_READ   = 51;
 static const int                        ERR_UNABLE_OPEN_FILE_WRITE  = 52;
 static const int                        ERR_STREAM_ERROR            = 53;
-static const int                        ERR_UNABLE_SET_FILE_PARAM   = 53;
+static const int                        ERR_UNABLE_SET_FILE_PARAM   = 54;
+static const int                        ERR_CANT_GET_STAT           = 55;
 
 static const int                        ERR_WRONG_INPUT_PARAMETERS  = 60;
 
