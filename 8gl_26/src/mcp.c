5fc643a165d58434ccea209613badf2f6f21a3fa
Nick-Msk <vmksk.cur@gmail.com>
2026-04-25 10:01:56 +0300

    KR:  mcp for test p8.5 pg 187-190, parse_key macroses are moved into utils

--- 8gl_26/src/mcp.c
+++ 8gl_26/src/mcp.c
@@ -7,6 +7,7 @@
 #include "error.h"
 #include "checker.h"
 #include "files.p8.5.h"
+#include "parse_keys.h"
 
 typedef struct Keys {
     bool              version;    // bool example
@@ -17,42 +18,6 @@ typedef struct Keys {
 } Keys;
 
 #define                 Keysinit(...) (Keys){ .version = false, .unbuf = false, .infilename = 0, .outfilename = 0, __VA_ARGS__}
-
-#define                 parse_value(c, name, func)\
-case c:\
-    if (argv[0][1] == '\0') {\
-        if (argv[1]){\
-            ptr = argv[1];\
-            argv++;\
-        } else\
-             return userraise(-1, ERR_WRONG_PARAMETER, "-"#c" option without value (must be string followed), ex '-lstring' or '-l string'");\
-    } else\
-        ptr = argv[0] + 1;\
-    ke->name = func(ptr);\
-    argv[0] += strlen(argv[0]) - 1;\
-    params++;\
-break;
-
-// note - name must be a part of Keys structure, c must be char literal
-#define                 parse_string(c, name) parse_value(c, name, )
-
-// note - name must be a part of Keys structure, c must be char literal
-#define                 parse_int(c, name) parse_value(c, name, atoi)
-
-// note - name must be a part of Keys structure, c must be char literal
-#define                 parse_long(c, name) parse_value(c, name, atol)
-
-#define                 parse_bool(c, name)\
-case c:\
-     ke->name = true;\
-     params++;\
-break;
-
-#define                 parse_bool_false(c, name)\
-case c:\
-     ke->name = false;\
-     params++;\
-break;
 
 static int              parse_keys(const char *argv[], Keys *ke){
 
