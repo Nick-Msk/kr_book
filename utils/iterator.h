58e80a0fbd7196a58cb88b0cea1dad357fd701bf
Nick-Msk <vmksk.cur@gmail.com>
2026-05-01 13:29:40 +0300

    UTILS: iterator.h: init

---
+++ utils/iterator.h
@@ -0,0 +1,19 @@
+#ifndef _ITERATOR_H
+#define _ITERATOR_H
+
+#define foreachint(var, ...) \
+    for (int *_iter = (int[]){__VA_ARGS__, INT_MAX}, var = *_iter; \
+         var != INT_MAX; \
+         var = *++_iter)
+
+#define foreachlong(var, ...) \
+    for (long *_iter = (int[]){__VA_ARGS__, LONG_MAX}, var = *_iter; \
+         var != LONG_MAX; \
+         var = *++_iter)
+
+#define foreachstring(var, ...) \
+    for (const char **_iter = (int[]){__VA_ARGS__, 0x0}, var = *_iter; \
+         var != 0x0; \
+         var = *++_iter)
+
+#endif /* !_ITERATOR_H */
