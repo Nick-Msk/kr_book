acad66536d5c2dc0f19b84bad4d0f6464359b4c6
Nick-Msk <vmksk.cur@gmail.com>
2026-05-01 13:50:43 +0300

    UTILS: iterator.h: minor fix

--- utils/iterator.h
+++ utils/iterator.h
@@ -1,6 +1,8 @@
 #ifndef _ITERATOR_H
 #define _ITERATOR_H
 
+#include <limits.h>
+
 #define foreachint(var, ...) \
     for (int *_iter = (int[]){__VA_ARGS__, INT_MAX}, var = *_iter; \
          var != INT_MAX; \
@@ -13,7 +15,7 @@
 
 #define foreachstring(var, ...) \
     for (const char **_iter = (int[]){__VA_ARGS__, 0x0}, var = *_iter; \
-         var != 0x0; \
+         var != 0; \
          var = *++_iter)
 
 #endif /* !_ITERATOR_H */
