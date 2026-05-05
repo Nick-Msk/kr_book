7aaa32f887153bbab36ee527abc2bdd662420a54
Nick-Msk <vmksk.cur@gmail.com>
2026-05-05 12:45:13 +0300

    KR: custom  alloc, ver2, upd1 (NO_BUILD)

--- 8gl_26/alloc2/src/alloc2.c
+++ 8gl_26/alloc2/src/alloc2.c
@@ -19,8 +19,15 @@ typedef union Header {
 
 
 // storage
-static const int        ARR_MAX = 12;
-static const int        ARR_MAX = 1024 * 1024 * 64;    // 64mb
+static const int        ARR_MAX_CNT = 12;
+static const int        ARR_MAX     = 1024 * 1024 * 64;    // 64mb
 
-static char             g_arr[ARR_MAX][ARR_MAX];
+static char             g_arr[ARR_MAX_CNT][ARR_MAX]; // to avoid using mmap()
+static int             *g_ptr;  // pointer to current unallocate
+
+static void             init_data(int sz){
+    
+}
+
+
 
