f19e4dfec98f685eb3e40e1d83e39b3dfe0566a8
Nick-Msk <vmksk.cur@gmail.com>
2026-05-07 21:24:08 +0300

    UTILS: iterator.h: (p)foreach_arr are added (TEST_OK)

--- utils/iterator.h
+++ utils/iterator.h
@@ -28,22 +28,70 @@
          var != 0; \
          var = *++_iter)
 
-#define CONCAT_EXPAND(x, y) x ## y
-#define CONCAT(x, y)        CONCAT_EXPAND(x, y)
-#define UNIQUE_ID(prefix)   CONCAT(prefix, __COUNTER__)
+// ----------------------------------------- COMMON EXPAND MACRO -------------------------------------
 
-#define _foreach_pointer_impl(i, arr, iter_name) \
-        _Static_assert(__builtin_classify_type(*arr) == 5, \
+#define                     CONCAT_EXPAND(x, y) x ## y
+#define                     CONCAT(x, y)        CONCAT_EXPAND(x, y)
+#define                     UNIQUE_ID(prefix)   CONCAT(prefix, __COUNTER__)
+
+// ----------------------------- ARRAY OF POINTERS, RETURNS POINTER TO ELEMENT -----------------------
+#define                     _pforeach_arrptr_impl(pi, parr, iter_name) \
+        _Static_assert(__builtin_classify_type(*parr) == 5, \
                        "foreach_pointer: массив должен состоять из указателей"); \
-        for (typeof_unqual(*arr)* iter_name = (arr), i = *iter_name; \
+        for (typeof_unqual(*parr)* iter_name = (parr), pi = *iter_name; \
                   *iter_name != 0;\
-                  i = *++iter_name)
+                  pi = *++iter_name)
 
-#define _foreach_pointer_named(i, arr, arr_name) \
-    typeof_unqual(arr) arr_name = (arr); \
-    _foreach_pointer_impl(i, arr_name, UNIQUE_ID(_iter_))
+#define                     _pforeach_arrptr_named(pi, parr, parr_name) \
+    typeof_unqual(parr) parr_name = (parr); \
+    _pforeach_arrptr_impl(pi, parr_name, UNIQUE_ID(_iter_))
 
-#define foreach_pointer(i, arr) \
-    _foreach_pointer_named(i, arr, UNIQUE_ID(_arr_))
+// iterate via pointer to element
+#define                     pforeach_arrptr(pi, parr)\
+    _pforeach_arrptr_named(pi, parr, UNIQUE_ID(_parr_))
+
+// ----------------------------- ARRAY OF POINTERS, RETURNS INDEX -------------------------------------
+// iterate via int value
+#define                     foreach_arrptr(i, parr)\
+    _Static_assert(__builtin_classify_type(*(parr)) == 5, "Array of pointers is required");\
+    for (int i = 0; parr[i] != 0; i++)
+
+// -------------------------------- ANY ARRAY, RETURN INDEX, BUT CNT MUST BE SUPPLIED -----------------
+#define                     foreach_arr(i, cnt)\
+    for (int i = 0; i < (cnt); i++)
+
+// ---------------- ARRAY OF POINTERS, RETURNS ELEMENT, BUT CNT MUST BE SUPPLIED  --------------------
+
+// Внутренний макрос: получает уже готовое arr_name и iter_name
+#define                             _foreach_arr_impl(iter, arr_name, cnt) \
+    for (typeof_unqual(*(arr_name)) *iter_name = (arr_name), iter = *iter_name; \
+         iter_name - arr_name <cnt; \
+         iter = *++iter_name)
+
+// Промежуточный макрос: сохраняет arr в arr_name (полученном снаружи) и генерирует iter_name
+#define                             _foreach_arr_named(iter, arr, cnt, arr_name) \
+    typeof_unqual(*arr) *arr_name = arr; \
+    _foreach_arr_impl(iter, arr_name, cnt)
+
+// Внешний макрос: генерирует уникальное имя для arr ОДИН раз и передаёт его вниз
+#define                             foreach_arr(iter, arr, cnt) \
+    _foreach_arr_named(iter, (arr), (cnt), UNIQUE_ID(_arr_))
+
+// ----------- ARRAY OF POINTERS, RETURNS POINTER TO ELEMENT, BUT CNT MUST BE SUPPLIED  ---------------
+
+// Внутренний макрос: получает уже готовое arr_name и iter_name
+#define                             _pforeach_arr_impl(iter, arr_name, cnt) \
+    for (typeof_unqual(*(arr_name)) *iter_name = (arr_name), *iter = iter_name; \
+         iter_name - arr_name <cnt; \
+         iter = ++iter_name)
+
+// Промежуточный макрос: сохраняет arr в arr_name (полученном снаружи) и генерирует iter_name
+#define                             _pforeach_arr_named(iter, arr, cnt, arr_name) \
+    typeof_unqual(*arr) *arr_name = arr; \
+    _pforeach_arr_impl(iter, arr_name, cnt)
+
+// Внешний макрос: генерирует уникальное имя для arr ОДИН раз и передаёт его вниз
+#define                             pforeach_arr(iter, arr, cnt) \
+    _pforeach_arr_named(iter, (arr), (cnt), UNIQUE_ID(_arr_))
 
 #endif /* !_ITERATOR_H */
