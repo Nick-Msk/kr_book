f40ea5815da0faf2f377307011ee670c66cd7fdd
Nick-Msk <vmksk.cur@gmail.com>
2026-05-07 11:14:26 +0300

    UTILS: iterator.h:  foreach_pointer iterator is added

--- utils/iterator.h
+++ utils/iterator.h
@@ -28,4 +28,22 @@
          var != 0; \
          var = *++_iter)
 
+#define CONCAT_EXPAND(x, y) x ## y
+#define CONCAT(x, y)        CONCAT_EXPAND(x, y)
+#define UNIQUE_ID(prefix)   CONCAT(prefix, __COUNTER__)
+
+#define _foreach_pointer_impl(i, arr, iter_name) \
+        _Static_assert(__builtin_classify_type(*arr) == 5, \
+                       "foreach_pointer: массив должен состоять из указателей"); \
+        for (typeof_unqual(*arr)* iter_name = (arr), i = *iter_name; \
+                  *iter_name != 0;\
+                  i = *++iter_name)
+
+#define _foreach_pointer_named(i, arr, arr_name) \
+    typeof_unqual(arr) arr_name = (arr); \
+    _foreach_pointer_impl(i, arr_name, UNIQUE_ID(_iter_))
+
+#define foreach_pointer(i, arr) \
+    _foreach_pointer_named(i, arr, UNIQUE_ID(_arr_))
+
 #endif /* !_ITERATOR_H */
