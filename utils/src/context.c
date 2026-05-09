11f589692bfd09a547d51b48111520b714d6dd5d
Nick-Msk <vmksk.cur@gmail.com>
2026-05-09 22:12:42 +0300

    UTILS: context.h: api upd6 (TEST_OK)

--- utils/src/context.c
+++ utils/src/context.c
@@ -12,7 +12,7 @@
 #include "iterator.h"
 
 /********************************************************************
-                    CONTEXT STRING MODULE IMPLEMENTATION
+                    CONTEXT MODULE IMPLEMENTATION
 ********************************************************************/
 
 static const int                CTX_MAX_NAME_LEN    = 8192;
@@ -27,7 +27,8 @@ static unsigned             get_hash(const Context *c, const char *str){
 
 static void                 free_elements(ContextSortedElem *els){
     int  cnt = 0;
-    for (ContextSortedElem *p = els, *n; p != 0; p = n->next){
+    for (ContextSortedElem *p = els, *n = 0; p != 0; p = n /* next */){
+        logsimple("p %p, n = %p", p, n);
         n = p->next;
         free(p->name), free(p->value);
         free(p); cnt++;
@@ -36,12 +37,15 @@ static void                 free_elements(ContextSortedElem *els){
 }
 
 static ContextSortedElem   *getprev(const Context *restrict c, const char *restrict name, unsigned *restrict phash){
-    logenter("Loopup %s", name);
+    logenter("Loopup [%s]", name);
 
     ContextSortedElem *el = 0, *prevel = 0;
     unsigned hash;
-    for (el =c->ctx[hash = get_hash(c, name)]; el != 0 && strcmp(el->name, name) < 0; el = el->next)
+    for (prevel = el =c->ctx[hash = get_hash(c, name)]; el != 0 && strcmp(el->name, name) < 0; el = el->next){
+        logmsg("el %p, hash %u", el, hash);
         prevel = el;
+    }
+    logmsg("after: el %p, hash %u", el, hash);
     if (phash)
         *phash = hash;
     return logret(prevel, "Found prev %p", prevel);   // < or = in the list
@@ -120,10 +124,9 @@ ContextSortedElem    *ctxget(const Context *restrict c, const char *restrict nam
     invraise(c != 0 && name != 0, "Null pointer");
     logenter("Loopup %s", name);
 
-    ContextSortedElem *prevel = getprev(c, name, 0), *el = 0;
-    logmsg("prev = %p, next = %p", prevel, prevel ? prevel->next : 0);
+    ContextSortedElem *el = getprev(c, name, 0);
 
-    if (prevel != 0 && (el = prevel->next) != 0 && strcmp(el->name, name) == 0)
+    if (el != 0 && strcmp(el->name, name) == 0)
          return logret(el, "Found %p", el);   // exact
     return logerr( (ContextSortedElem *) 0, "Not found");
 }
@@ -169,7 +172,7 @@ int                          ctx_fprintelem(FILE *restrict out, const ContextSor
     invraise(elem != 0, "Null pointer");
     int         cnt = 0;
     if (out)
-        cnt += fprintf(out, "%d[%.20s:%.100s]%s\n", elem->flags, elem->name, elem->value, elem->next ? "->": "");   // TODO: 20 and 100 can be configurable
+        cnt += fprintf(out, "%d [%.20s:%.100s]%s\n", elem->flags, elem->name, elem->value, elem->next ? "->": "");   // TODO: 20 and 100 can be configurable
     return cnt;
 }
 
@@ -178,15 +181,15 @@ int                          ctx_techfprint(FILE *restrict out, const Context *r
     int cnt = 0;
     if (out){
         cnt += fprintf(out, "CONTEXT %s[%d - %p]:", name, c->cnt, c->ctx);
-        /* foreach_arr(item, c->ctx, c->cnt){
+        foreach_arr(item, c->ctx, c->cnt){
             if (item)
                 cnt += printlist(out, item);
-        } */
-        for (int i = 0; i < c->cnt; i++)
+        }
+        /*for (int i = 0; i < c->cnt; i++)
             if (c->ctx[i]){
                 fprintf(out, "%3d: ", i);
                 cnt += printlist(out, c->ctx[i] );
-            }
+            }*/
         cnt += fprintf(out, "\n");
     }
     return cnt;
@@ -244,7 +247,7 @@ tf2(const char *name)
 
         test_validatefree(res != 0, ctxfree(c), "Not found - error\n");
 
-      //  test_validatefree(strcmp(res, value) == 0, ctxfree(c), "value [%s] must be equal to init value [%s]", res, value);
+        test_validatefree(strcmp(res, value) == 0, ctxfree(c), "value [%s] must be equal to init value [%s]", res, value);
 
         ctxfree(c);
     }
@@ -261,12 +264,13 @@ tf3(const char *name)
 
     test_sub("subtest %d: add + get + del", ++subnum);
     {
-        int     initcnt = 100;
+        int     initcnt = 10;
         Context c = ctxinit(initcnt);
         test_validatefree(c.cnt > initcnt, ctxfree(c), "cnt %d  must be prime number and >= %d", c.cnt, initcnt);
 
-        const char *name = "Test name 1", *value = "Test value 1";
+        const char *name = "Test name xxx", *value = "Test value yyy";
         ctxadd(&c, name, value);
+        
         ctxtechfprint(logfile, c);
 
         const char *res = ctxgetvalue(&c, name);
@@ -280,6 +284,26 @@ tf3(const char *name)
     }
     return logret(TEST_PASSED, "done"); // TEST_FAILED, TEST_PASSED, TEST_MANUAL
 }
+
+// ------------------------- TEST 4 ---------------------------------
+
+static TestStatus
+tf4(const char *name)
+{
+    logenter("%s", name);
+    int         subnum = 0;
+
+    int     initcnt = 10;
+    Context c = ctxinit(initcnt);
+    test_sub("subtest %d: multiple add", ++subnum);
+    {
+        // TODO:
+        ctxtechfprint(logfile, c);
+    }
+    ctxfree(c);
+    return logret(TEST_PASSED, "done"); // TEST_FAILED, TEST_PASSED, TEST_MANUAL
+}
+
 // ------------------------------------------------------------------------------------------------------------------------------
 int
 main( /* int argc, const char *argv[] */)
@@ -289,7 +313,8 @@ main( /* int argc, const char *argv[] */)
     testenginestd(
         testnew(.f2 = tf1, .num = 1, .name = "Simple init and validate test"              , .desc=""                , .mandatory=true)
       , testnew(.f2 = tf2, .num = 2, .name = "Simple add and get test"                    , .desc=""                , .mandatory=true)
-      //, testnew(.f2 = tf3, .num = 3, .name = "Simple add + get + del test"                , .desc=""                , .mandatory=true)
+      , testnew(.f2 = tf3, .num = 3, .name = "Simple add + get + del test"                , .desc=""                , .mandatory=true)
+      , testnew(.f2 = tf4, .num = 4, .name = "Multiple add + get test"                    , .desc=""                , .mandatory=true)
     );
 
     return logret(0, "end...");  // as replace of logclose()
