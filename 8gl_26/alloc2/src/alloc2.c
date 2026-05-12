3f9ffa04ec828029c54f4e6f0dd059bf77bf480c
Nick-Msk <vmksk.cur@gmail.com>
2026-05-12 22:13:29 +0300

    KR: custom  alloc, ver2, upd1 (BUILD_OK)

--- 8gl_26/alloc2/src/alloc2.c
+++ 8gl_26/alloc2/src/alloc2.c
@@ -9,27 +9,30 @@ typedef long Align;
 
 typedef union Header {
     struct {
-        union Header   *ptr;
+        union Header   *freeptr;
         unsigned        size;
     };
     Align               not_used;
 } Header;
 
+#define                 HeaderInit(...) (Header) {.freeptr = 0, .size = 0, ##__VA_ARGS__ }
+
 typedef struct Control {
-    void               *ptr;   // to g_arr + i etc
-    size_t              total;
-    size_t              free;
+    Header             *freeptr;   // to g_arr + i etc
+    unsigned            total;  // units!!!
+    unsigned            free;   // units!!!
 } Control;
 
 // storage
-static const int        ARR_MAX_CNT = 12;
-static const int        ARR_MAX     = 1024 * 1024 * 64;    // 64mb
+static const int        ARR_MAX_CNT     = 12;
+static const int        ARR_MAX         = 1024 * 1024;    // 1mb of Header
+static const int        ARR_MAX_UNIT    = ARR_MAX / sizeof(Header);
 
 // never user that array directly, only via control structure
-static char             g_arr[ARR_MAX_CNT][ARR_MAX]; // to avoid using mmap()
-//static int             *g_ptr;  // pointer to current unallocated!
+static Header          g_arr[ARR_MAX_CNT][ARR_MAX]; // to avoid using mmap()
+static int              g_ptr;  // pointer to current unallocated!
 
-#define                 ControlInit(N) (Control) {.ptr = g_arr + ARR_MAX * (N), .total = ARR_MAX / sizeof(Header), .free = ARR_MAX / sizeof(Header) }
+#define                 ControlInit(N) (Control) {.freeptr = (Header *)g_arr + ARR_MAX_UNIT * (N), .total = ARR_MAX_UNIT, .free = ARR_MAX_UNIT }
 
 static Control          g_control[ARR_MAX_CNT] =
 {   ControlInit(0),
@@ -48,25 +51,62 @@ static Control          g_control[ARR_MAX_CNT] =
 
 // ------------------------------- Utilities -------------------------------------
 
+static void                 initlocation(Header *ptr, unsigned sz){
+    *(Header *) ptr = HeaderInit(.freeptr = ptr + 1, .size = sz - 1);    // -1 for header
+}
+
+static inline bool          checklimitlocation(Header *ptr, int loc){
+    return (char *) ptr < (char *) g_control[loc].freeptr + g_control[loc].total;
+}
+
+static void                 updatelocation(int loc, unsigned cntu, bool alloc){
+    if (alloc)  // -=
+        g_control[loc].free -= cntu;
+    else
+        g_control[loc].free += cntu;
+    invraise(g_control[loc].free <= g_control[loc].total, "Out of range free %u: total %d", g_control[loc].free, g_control[loc].total);
+}
+
+static inline unsigned      calc_units(unsigned bytes){
+    return (bytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+}
+
 // loc < 12
 static void                *findlocation(int loc, unsigned bytes){
     invraise(loc >= 0 && loc < ARR_MAX_CNT, "Out of loc %d", loc);
 
+    logenter("loc %d: %u", loc, bytes);
     // converting into units
-    unsigned nu = (bytes + sizeof(Header) - 1) / sizeof(Header) + 1;
-    void        *ret = 0;
+    unsigned nu = calc_units(bytes);
+    logauto(nu);
+
+    if (g_ptr == loc)       // 1-st alloc this, need to formar
+        initlocation(g_control[loc].freeptr, ARR_MAX_UNIT), g_ptr++;
 
     if (g_control[loc].free >= nu){
 
-        char        *pos = g_control[loc].ptr;
-        logauto(nu);
-        ret = pos;  // init
+        Header         *pos = g_control[loc].freeptr;
+        Header         *prev = 0;
+        for (; pos && checklimitlocation(pos, loc); prev = pos, pos = pos->freeptr){
+            if (pos->size >= nu){
+                if (pos->size == nu){       // exact!
+                    if (prev)
+                        prev->freeptr = pos->freeptr;
+                    else        // 1-st iter, use g_control[loc].ptr instead
+                        g_control[loc].freeptr = pos->freeptr;
+                } else  {   // alloc area at the and of free space TODO: 
 
-        // if found update fee
-        if (ret)
-            g_control[loc].free -= nu;
+                
+                }
+                // setup Header *pos
+                pos->size = nu;     // to check in afree()
+                updatelocation(loc, nu, true);
+                return logret( (void *) (pos + 1), "Allocated %u", nu);  // header remains
+            }
+        }
+
     }
-    return logsimpleret(ret, "%u units allocated", nu);
+    return logerr( (void *) 0, "Unable to alloc %u units", nu);
 }
 
 // ------------------------------------- API -------------------------------------
