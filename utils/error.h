9242c86212dc27ef88448766d5d9499b05e63481
Nick-Msk <vmksk.cur@gmail.com>
2026-05-27 15:46:50 +0300

    UTILS: error.h: ERR_UNABLE_LOAD_FSARRAY is added

--- utils/error.h
+++ utils/error.h
@@ -49,6 +49,8 @@ static const int                        ERR_TOO_LONG_LINE           = 10010;
 static const int                        ERR_GUARD_RAISE             = 10100;
 static const int                        ERR_ACTION_NOT_APPLICABLE   = 10200;
 
+static const int                        ERR_UNABLE_LOAD_FSARRAY     = 10220;
+
 static const int                        ERR_UNABLE_TO_EXEC_FILE     = 10300;
 static const int                        ERR_UNABLE_TO_RUN_MAKE      = 10301;
 
