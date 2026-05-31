fb0e5c16518b5b3c84024e6e3092ee2143081dea
Nick-Msk <vmksk.cur@gmail.com>
2026-05-31 21:03:28 +0300

    UTILS: numeric_ops: hash_long() is added

--- utils/numeric_ops.h
+++ utils/numeric_ops.h
@@ -47,5 +47,13 @@ extern unsigned long        hash_fnv1a(const char *str);
 
 extern unsigned long        hash_sdbm(const char *str);
 
+static inline uint64_t      hash_long(int64_t x) {
+    uint64_t z = (uint64_t) x;
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ULL;    // Magic number omg
+    z = (z ^ (z >> 27)) * 0x94d049bb133111ebULL;
+    z = z ^ (z >> 31);
+    return z;
+}
+
 #endif /* !_NUMERIC_OPS_H */
 
