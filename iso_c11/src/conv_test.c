2cf7ea6ae0f0414807837574787f04b8736728b9
vmksk.cur@gmail.com <Nick-Msk>
2026-06-01 21:21:28 +0300

    ISO: int conversion testing upd1

--- iso_c11/src/conv_test.c
+++ iso_c11/src/conv_test.c
@@ -1,10 +1,10 @@
 #include <stdint.h>
 #include <stdio.h>
-
+#include <inttypes.h>
 
 int     main(void){
     int i = -1;
     uint64_t v1 = (uint64_t) i;
     uint64_t v2 = (uint64_t)(int64_t) i;
-    printf("v1 = %llu, v2 = %llu\n", v1, v2);
+    printf("v1 = %" PRIu64", v2 = %" PRIu64 "\n", v1, v2);
 }
