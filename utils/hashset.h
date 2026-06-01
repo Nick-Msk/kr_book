b205b0e2c53ff616793554c219f844e43ffe4773
Nick-Msk <vmksk.cur@gmail.com>
2026-06-01 10:19:35 +0300

    UTILS: hashset: upd2 (TEST_OK)

--- utils/hashset.h
+++ utils/hashset.h
@@ -19,21 +19,32 @@ typedef enum hset_type
     { HSET_INT = 1, HSET_LONG, HSET_DBL, HSET_FS, HSET_PTR }
 hset_type;
 
+static inline const char            *hset_type_name(hset_type t){
+    switch (t){
+        CASE_RETURN(HSET_INT);
+        CASE_RETURN(HSET_LONG);
+        CASE_RETURN(HSET_DBL);
+        CASE_RETURN(HSET_FS);
+        CASE_RETURN(HSET_PTR);
+        default: return "";
+    }
+}
+
 typedef struct hset_elem {
     union {
-        int                 val;
+        int                 ival;
         long                lval;    // can be ANY type
         double              dval;
         fs                  fsval;  // NOT implemented yet
-        void               *vval;
+        void               *pval;
     };
     struct hset_elem   *next;
 } hset_elem;
 
 typedef struct hset {
-    int         sz;     // init size!
-    int         flags;  // empty for now
-    hset_elem  *table;
+    int             sz;     // init size!
+    int             flags;  // empty for now
+    hset_elem     **table;
 } hset;
 
 #define                 HSET(size, typ) (hset) {.sz = (size), .flags = (typ), .table = 0 }
@@ -41,11 +52,11 @@ typedef struct hset {
 // ------------- CONSTRUCTOTS/DESTRUCTORS ----------
 
 extern hset             hset_init(int sz, hset_type typ);      // #define will be for particular type
-extern hset             hset_reinit(hset *s, int newsz);
+extern hset             hset_reinit(hset *se, int newsz);
 //
-extern void             hset_free(hset *s);
+extern void             hset_free(hset *se);
 //
-extern hset             hset_clone(const hset *s);
+extern hset             hset_clone(const hset *se);
 extern hset             hset_fromiarr(const int *iarr);  // iarr must be null-ended array
 // extern hset             hset_fromfsarr(fs_array iarr);   not supported yet
 extern hset             hset_fromlarr(const long *larr);
@@ -53,18 +64,20 @@ extern hset             hset_fromlarr(const long *larr);
 // -------------------- ACCESS AND MODIFICATORS ------------------------
 
 // ------------------------ Element access -----------------------------
-extern bool             hset_iset(hset *s, int val);
-extern bool             hset_lset(hset *s, long val);
+extern bool             hset_iset(hset *se, int val);
+extern bool             hset_lset(hset *se, long val);
 
-extern bool             hset_iremove(hset *s, int val);
-extern bool             hset_lremove(hset *s, long val);
+extern bool             hset_iremove(hset *se, int val);
+extern bool             hset_lremove(hset *se, long val);
 
-extern bool             hset_iget(const hset *s, int val);
-extern bool             hset_lget(const hset *s, long val);
+extern bool             hset_iget(const hset *se, int val);
+extern bool             hset_lget(const hset *se, long val);
 
 // ------------------------ PRINTERS/CHECKERS --------------------------
 
-extern int              hset_techfprint(const FILE *restrict out, const hset *s, int cnt);
-static inline int       hset_techprint(const hset *s, int cnt);
+extern int              hset_techfprint(FILE *restrict out, const hset *se, int cnt);
+static inline int       hset_techprint(const hset *se, int cnt);
+
+extern bool             hset_validate(FILE *out, const hset *restrict se);
 
 #endif /* !_HASHSET_H */
--- utils/src/hashset.c
+++ utils/src/hashset.c
@@ -18,14 +18,43 @@
 
 // ---------------------------- (Utility) printers -----------------------------
 
+int                         sortedlist_fprint(FILE *restrict out, const hset_elem *restrict elem, hset_type typ){
+    int     cnt = 0;
+    while (elem){
+        switch (typ){
+            case HSET_INT:
+                cnt += fprintf(out, "%d", elem->ival);
+            break;
+            case HSET_LONG:
+                cnt += fprintf(out, "%ld", elem->lval);
+            break;
+            case HSET_DBL:
+                cnt += fprintf(out, "%lf", elem->dval);
+            break;
+            case HSET_PTR:
+                cnt += fprintf(out, "%p", elem->pval);
+            break;
+            case HSET_FS:
+                cnt += fs_fprint(out, &elem->fsval, 0);
+            break;
+        }
+        cnt += fprintf(out, "\t");
+        elem = elem->next;
+    }
+    return cnt;
+}
+
 // ------------------------------------ Utilities ------------------------------------------
 
 // for int & long
-static unsigned             get_lhash(const hset *s, long val){
-    invraise(s != 0, "Null pointer");
-    return hash_long(val) % s->sz;
+static unsigned             get_lhash(const hset *se, long val){
+    invraise(se != 0, "Null pointer");
+    return hash_long(val) % se->sz;
 }
 
+static inline hset_type     getype(const hset *se){
+    return se->flags & 0xFF;
+}
 
 // ------------------------------------- API -----------------------------------------------
 
@@ -33,18 +62,84 @@ static unsigned             get_lhash(const hset *s, long val){
 
 hset                        hset_init(int sz, hset_type typ){
     logenter("init sz %d", sz);
+
+    if (int_notin(typ, HSET_INT, HSET_LONG, HSET_DBL, /* HSET_FS, */ HSET_PTR) )
+        userraiseint(ERR_UNSUPPORTED_TYPE, "%d", typ);
+
     unsigned    newsz = next_prime(sz);
     hset res = HSET(newsz, typ);
-    if ( (res.table = malloc(newsz * sizeof(hset_elem) ) ) == 0)    // raise here - nothing to do
+    if ( (res.table = malloc(newsz * sizeof(hset_elem *) ) ) == 0)    // raise here - nothing to do
         userraiseint(ERR_UNABLE_ALLOCATE, "Unable to alloc newsz %u elems", newsz);
+    clean_ptr( (void *) res.table,  newsz);
+
     return logret(res, "Crreated with %d", res.sz);
 }
 
+void                        hset_free(hset *s){
+    invraise(s != 0, "Null pointer");
+    switch (s->flags & 0xFF){
+        case HSET_INT: case HSET_LONG: case HSET_DBL: case HSET_PTR:
+        break;
+        case HSET_FS:
+            userraiseint(ERR_UNSUPPORTED_TYPE, "HSET_FS (%d) is'nt suppored for now", HSET_FS);
+        break;
+        default:
+            userraiseint(ERR_UNSUPPORTED_TYPE, "Unknown type %d", s->flags & 0xFF);
+        break;
+    }
+    free(s->table);
+    logsimple("freed");
+    s->sz = s->flags = 0;
+}
 
+// -------
+bool                        hset_validate(FILE *out, const hset *restrict se){
+    logenter("...");
+    if (!se){    // TODO: think about string creation via faststring here!!
+        if (out)
+            fprintf(out, "null pointer");
+        return logerr(false, "null pointer");
+    }
+    if (se->sz <= 0){
+        if (out)
+            fprintf(out, "Incorrect sz (%d)", se->sz);
+        return logerr(false, "Incorrect sz (%d)", se->sz);
+    }
+    if (int_notin(getype(se), HSET_INT, HSET_LONG, HSET_DBL, HSET_FS, HSET_PTR) ){
+        if (out)
+            fprintf(out, "Incorrect type %d", getype(se) );
+        return logerr(false, "Incorrect type %d", getype(se) );
+    }
+    if (!se->table){
+        if (out)
+            fprintf(out, "null hashtable");
+        return logerr(false, "null hashtable");
+    }
+    return logret(true, "Validated");
+}
 
 // ---------------------------------- General functions ------------------------------------
 
+
 // ------------------------------------- (API) printers ------------------------------------
+
+int                         hset_techfprint(FILE *restrict out, const hset *se, int sz){
+    invraise(se != 0, "Null pointer");
+    int     cnt = 0;
+    if (out){
+        sz = sz ? MIN(sz, se->sz) : se->sz;
+        logauto(sz);
+        cnt += fprintf(out, "HSET (%d:%s) [\n", se->sz, hset_type_name(getype(se) ) );
+        for (int i = 0; i < sz; i++)
+            if (se->table[i]){
+                cnt += fprintf(out, "%4d: ", i);
+                cnt += sortedlist_fprint(out, se->table[i], getype(se) );
+                cnt += fprintf(out, "\n");
+            }
+        cnt += fprintf(out, "]\n");
+    }
+    return cnt;
+}
 
 // --------------------------------- SERIALIZATION -----------------------------------------
 
@@ -52,7 +147,6 @@ hset                        hset_init(int sz, hset_type typ){
 #ifdef HSETTESTING
 
 #include "test.h"
-#include "checker.h"
 
 //types for testing
 
@@ -64,9 +158,14 @@ tf1(const char *name)
     logenter("%s", name);
     int         subnum = 0;
 
-    test_sub("subtest %d: ........", ++subnum);
+    test_sub("subtest %d: init + free", ++subnum);
     {
-        
+        hset se1 = hset_init(100, HSET_INT);
+        hset_techfprint(logfile, &se1, 0);
+        test_validatefree(
+            hset_validate(stdout, &se1), hset_free(&se1), "Validation failed"
+        );
+        hset_free(&se1);
     }
     return logret(TEST_PASSED, "done"); // TEST_FAILED, TEST_PASSED, TEST_MANUAL
 }
