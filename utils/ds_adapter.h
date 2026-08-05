28d2383aafd1f55705178d5909b0a784f0b73c2c
Nick-Msk <vmksk.cur@gmail.com>
2026-08-05 17:42:54 +0300

    UTILS: ds_adapter: init , dsPrintf() is added (NO_TEST)

--- utils/ds_adapter.h
+++ utils/ds_adapter.h
@@ -9,11 +9,14 @@
  * DS_STR               alloes only R, and W only limited by '\0'. No reallocation.
  */
 
-#ifndef _DS_FS_H
-#define _DS_FS_H
+#ifndef _DS_ADAPTER_H
+#define _DS_ADAPTER_H
+
+#include <stdarg.h>
 
 #include "ds.h"
 #include "checker.h"
+#include "common.h"
 
 // ---------------------------------------------------------------------------------
 // --------------------------- Public Ds - fs adapter API --------------------------
@@ -27,45 +30,23 @@
 
 // -------------------- ACCESS AND MODIFICATORS -------------------------------------
 
-// Ds - fs adapter NOT SURE
-typedef union {
-    Ds ds;
-    struct {
-        DSType type;                /**< Determines which union member is active. */
-        fs     s;
-    };
-} Dsfs;
-
 /**
  * @brief Ds printf wrapper
  * @note DS_FILE, DS_FS, DS_STR are allowed
  * @note This is stream function, always used ds->pos as start point
  */
-int                         dsPrintf(Ds *ds, const char *msg, ...) __attribute__ ((format (printf, 2, 3)));
+extern int                         dsPrintf(Ds *restrict ds, const char *restrict msg, ...) __attribute__ ((format (printf, 2, 3)));
 
 /**
  * @brief Ds ысфта wrapper
  * @note DS_FILE, DS_FS, DS_CONSTSTR, DS_STR are allowed
  * @note This is stream function, always used ds->pos as start point
  */
-int                         dsScanf(Ds *ds, const char *msg, ...) __attribute__ ((format (scanf, 2, 3)));
-
-/**
- * @brief Stream reset function
- *//*
-static inline bool          dsReset(Ds *ds) {
-    invraisecode(ds != NULL, ERR_NULLABLE_PTR, "Null ds");
-    switch (ds->type) {
-        case DS_FILE:
-            return ftell(ds->fp,  0L, SEEK_SET) == 0;
-        case DS_FS: 
-
-    }
-}*/
+extern int                         dsScanf(Ds *restrict ds, const char *restrict msg, ...) __attribute__ ((format (scanf, 2, 3)));
 
 // ------------------------ PRINTERS/CHECKERS ---------------------------------------
 
 // ------------------------------------ ETC. ----------------------------------------
 
 
-#endif /* !_DS_FS_H */
+#endif /* !_DS_ADAPTER_H */
---
+++ utils/src/ds_adapter.c
@@ -0,0 +1,112 @@
+
+#include "ds_adapter.h"
+
+/********************************************************************
+                 Ds - fs adapter MODULE IMPLEMENTATION
+********************************************************************/
+
+// ------------------------------ Utilities ------------------------
+
+/**
+ * @brief Internal helper to print formatted string into a fixed-size buffer.
+ * 
+ * This function calculates the required size first, then writes the content.
+ * It ensures that the buffer does not overflow and handles null-termination.
+ *
+ * @param[in] ptr      Pointer to the start of the destination buffer.
+ * @param[in] pos      Current write position (offset) in the buffer.
+ * @param[in] cap      Total capacity of the buffer (limit).
+ * @param[in] fmt      Format control string.
+ * @param[in] ap       Variable argument list.
+ * 
+ * @return The number of characters written (excluding null terminator), 
+ *         or -1 if the buffer is too small or an error occurred.
+ */
+int                         dsVPrintStr(char *ptr, size_t pos, size_t cap, const char *fmt, va_list ap) {
+    int needed = vsnprintf(NULL, 0, fmt, ap);
+    if (needed < 0)
+        return userraise(-1, ERR_STREAM_ERROR, "Unable to vsnprintf NULL");
+    if ((size_t) needed + pos + 1 > cap)
+        return -1;
+
+    int written = vsnprintf(ptr + pos, cap - pos, fmt, ap);
+
+    if (written < 0) {
+        return userraise(-1, ERR_STREAM_ERROR, "Unable to vsnprintf");
+    }
+    return written;
+}
+
+// --------------------------- API ---------------------------------
+
+int                         dsPrintf(Ds *restrict pds, const char *restrict msg, ...) {
+    invraisecode(pds != NULL && msg != NULL, ERR_NULLABLE_PTR, 
+        "Null input %p %p", pds, msg);
+
+    va_list     ap;
+    int         total = 0;
+    va_start(ap, msg);
+    switch (pds->type) {
+        case DS_FILE:
+            IOCHECKER(wr, vfprintf(pds->fp, msg, ap), -1)
+                total += wr;
+            break;
+        case DS_STR: // this is NOT autoextendable, till end of pds->ptr only
+            IOCHECKER(wr, dsVPrintStr(pds->ptr, pds->pos, pds->cap, msg, ap), -1) {
+                total += wr;
+                pds->pos += wr;
+            }
+            break;
+        case DS_FS:     // this is autoextendable
+            IOCHECKER(wr, fs_sprintf_position(&pds->s, pds->pos, msg, ap), -1) {
+                total += wr;
+                pds->pos += wr; // iterator ovwe fs pds->s
+            }
+            break;
+        default:
+            va_end(ap); // for lulz
+            return userraise(-1, ERR_ACTION_NOT_APPLICABLE, 
+                "Can't write to %s", DSTypeName(pds->type) );
+    }
+    va_end(ap);
+    return total;
+}
+
+int                         dsScanf(Ds *restrict pds, const char *restrict msg, ...) {
+
+}
+
+// -------------------- CONSTRUCTOTS/DESTRUCTORS -------------------
+
+// -------------------------- (API) printers -----------------------
+
+// -------------------------------Testing --------------------------
+
+#ifdef DS_ADAPTER_TESTING
+
+#include "test.h"
+
+// ------------------------- TEST Ds (DataSource) -------------------------
+static TestStatus
+tf_ds(const char *name)
+{
+    logenter("%s", name);
+    int subnum = 0;
+
+    return logret(TEST_PASSED, "done");
+}
+
+// -------------------------------------------------------------------
+int
+main( /*int argc, char *argv[] */ )
+{
+    logsimpleinit("Start");
+
+    testenginestd(
+        TESTADD(tf_ds,           "Ds (DataSource) simple tests")
+    );
+
+    return logret(0, "end...");  // as replace of logclose()
+}
+
+#endif /* DS_FS_ADAPTER_TESTING */
