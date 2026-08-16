cc304ff5387dbb6da07230bd55a3a3ce81a0ebd9
Nick-Msk <vmksk.cur@gmail.com>
2026-08-16 19:44:32 +0300

    UTILS: v64typed: v64typedCreateFsSource() is added

--- utils/v64typed.h
+++ utils/v64typed.h
@@ -41,8 +41,12 @@ typedef struct {
 static inline v64typed              v64typedCreate(value64 val, value64_type typ) {
     return V64TYPEDZERO(.val = val, .typ = typ);
 }
-// not get ownership
+// not get ownership, for generators
 static inline v64typed              v64typedCreateCstrSource(const char *str) {
+    return v64typedCreate(LITERAL64_PTR( (void *) str), VALUE64_PTR); // PTR for simulation!
+}
+// not get ownership, for generators
+static inline v64typed              v64typedCreateFsSource(const fs *str) {
     return v64typedCreate(LITERAL64_PTR( (void *) str), VALUE64_PTR); // PTR for simulation!
 }
 static inline v64typed              v64typedCreateInt(int x) {
